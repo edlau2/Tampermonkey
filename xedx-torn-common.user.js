014cf1ae0e899e6fefd060ca6e34d70e34e6002f
edlau2 <edlau@ntplx.net>
2019-03-05 15:26:00 -0500

    Fix headers
    Attempt to fix headers for submission to openuserjs.org

--- xedx-torn-common.user.js
+++ xedx-torn-common.user.js
@@ -1,16 +1,33 @@
+/*
+Copyright 2019+, Ed Lau, xedx [2100735]
+
+xedx-torn-config is distributed under the terms of the GNU Lesser General Public License.
+    GM_config is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 // ==UserScript==
 // @exclude       *
-// @author        xedx [2100735]
+// @author        edlau
 
 // ==UserLibrary==
 // @name          xedx-torn-common
-// @author        xedx [2100735]
+// @author        edlau
 // @description   A collection of utilities and structures for inclusion in Torn user scripts.
 // @copyright     2019+, xedx [2100735]
-// @license       LGPL-3.0-or-later
-// ==/UserLibrary==
+// @license       LGPL-3.0-or-later; http://www.gnu.org/licenses
 
 // ==/UserScript==
+
+// ==/UserLibrary==
 
 // Create a timestamp string for current time (YY-MM-DD HH:MM:SS)
 function timestamp() {
