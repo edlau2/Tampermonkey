19f8189c6af5d07243a469cd79b1917ceee006b8
edlau2 <edlau@ntplx.net>
2019-10-17 20:04:41 -0400

    Add files via upload

--- WindowSizeSample/window_size.user.js
+++ WindowSizeSample/window_size.user.js
@@ -1,90 +1,46 @@
 // ==UserScript==
-// @name         Torn Numeric Rank Display
+// @name         Window Size
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Appends the numeric rank to a user's profile rank name
-// @author       xedx [2100735]
-// @include      https://www.torn.com/profiles.php*
+// @description  Resize the 'docks' window
+// @author       You
+// @match        https://www.torn.com/shops.php?step=docks
 // @grant        none
 // ==/UserScript==
 
-(function($) {
+// ^^ the 'match' line, above, tells the script what page URL to run on.
+// I assume you can more than one.
+// To run on ALL pages, an extension, rather than a script, is a better option.
+// On my GitHub page, see Torn Loot Level and an example of that.
+// This will only run on the ocks page in the city
+
+(function() {
     'use strict';
 
-    //
-    // We need class length, I know of 'medium' and 'long'.
-    // For 'Professional', for example, we need to go from
-    // 'medium' to 'long' to get it to fit. 'Star', however,
-    // can remain at 'medium'. 'Highly distinguished' does
-    // not fit into 'long', I don't know if there is anything
-    // beyond 'long', so for really long ranks, I truncate.
-    // This is set up so any attribute could be set, not only
-    // 'class' but perhaps 'font-size' instead.
-    //
-    // ['str-to-match', 'str-to-replace-with', 'attr', 'attr-value']
-    //
-    var ranks = [['Absolute beginner', 'Absolute noob', 'class', 'long'],
-                 ['Beginner', 'Beginner', 'class','medium'],
-                 ['Inexperienced', 'Inexperienced', 'class', 'long'],
-                 ['Rookie', 'Rookie', 'class','medium'],
-                 ['Novice', 'Novice', 'class','medium'],
-                 ['Below average', 'Below average', 'class', 'long'],
-                 ['Average', 'Average', 'class','medium'],
-                 ['Reasonable', 'Reasonable', 'class', 'long'],
-                 ['Above average', 'Above average', 'class', 'long'],
-                 ['Competent', 'Competent', 'class','medium'],
-                 ['Highly competent', 'Highly comp.', 'class', 'long'],
-                 ['Veteran', 'Veteran', 'class','medium'],
-                 ['Distinguished', 'Distinguished', 'class', 'long'],
-                 ['Highly distinguished', 'Highly dist.', 'class', 'long'],
-                 ['Professional', 'Professional', 'class', 'long'],
-                 ['Star', 'Star', 'class','medium'],
-                 ['Master', 'Master', 'class','medium'],
-                 ['Outstanding', 'Outstanding', 'class', 'long'],
-                 ['Celebrity', 'Celebrity', 'class', 'medium'],
-                 ['Supreme', 'Supreme', 'class','medium'],
-                 ['Idolised', 'Idolised', 'class','medium'],
-                 ['Champion', 'Champion', 'class','medium'],
-                 ['Heroic', 'Heroic', 'class','medium'],
-                 ['Legendary', 'Legendary', 'class', 'long'],
-                 ['Elite', 'Elite', 'class','medium'],
-                 ['Invincible', 'Invincible', 'class', 'long']];
+    // So we can debug in Chrome Developer tools...
+    debugger;
 
-    function addNumericRank() {
-        var elemList = document.getElementsByClassName('two-row');
-        var element = elemList[0];
-        if (element == 'undefined' || typeof element == 'undefined') {
-            return;
-        }
-        var rank = element.firstChild;
-        var html = rank.innerHTML;
-        for (var i = 0; i < ranks.length; i++) {
-            if (html == ranks[i][0]) {
-                while(rank.attributes.length > 0) {
-                    rank.removeAttribute(rank.attributes[0].name);
-                }
-                rank.setAttribute(ranks[i][2], ranks[i][3]);
-                rank.innerHTML = ranks[i][1] + ' (' + i +')';
-                return;
-            }
-        }
+    // We need to define what window we want to resize.
+    // A page could have many windows embeded in it.
+    // window.top is the topmost in the windows hierachy,
+    // could also just use 'window', i.e. myWindow = window
+    // This is because you can any frame on a page instead.
+    var myWindow = window.top;
+
+    // Call our function that does the resizing. We could just call
+    // myWindow.resizeTo(250, 250); directly here instead.
+    resizeWin(myWindow);
+
+    // Our function to resize any window, that we just called above.
+    // If not called, it won't run. We pased it myindow, which inside the function
+    // is referenced as someWindow
+    function resizeWin(someWindow) {
+        // You'll se an error in the debugger, here, saying resiveWindow is not a function
+        // of a window. So I'm to change this line.
+        // someWindow.resizeWindow(250, 250);
+        someWindow.resizeTo(250, 250);
+
+        // Also not a 'window' function, and not needed anyway, so get rid of it.
+        //someWindow.focus();
     }
-
-    //////////////////////////////////////////////////////////////////
-    // Main. Using a MutationObserver allows us to be notified
-    // whenever the root of the 'User Profile' section (the
-    // <div id="profileroot"> section) changes/updates. It is
-    // probably over-aggressive to trigger on all 3 actions...
-    //////////////////////////////////////////////////////////////////
-
-    console.log("Numeric Rank Display script started!");
-
-    var targetNode = document.getElementById('profileroot');
-    var config = { attributes: true, childList: true, subtree: true };
-    var callback = function(mutationsList, observer) {
-        addNumericRank();
-    };
-    var observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-
 })();
