e677445ef2a451fee9e3db3f6eab396c3506f91b
edlau2 <edlau@ntplx.net>
2020-03-21 14:59:08 -0400

    Add files via upload

--- helpers/Torn-JS-Helpers.js
+++ helpers/Torn-JS-Helpers.js
@@ -11,9 +11,16 @@
 // ==/UserLibrary==
 
 // ==/UserScript==
+initialize();
+
+function initialize() {
+  Number.prototype.format = function (n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+  };
+}
 
 function validateApiKey() {
-    
 }
 
 /*
