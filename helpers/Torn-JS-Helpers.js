2974f6f0aeffa96ce9632c6becbe4a789dc06b78
edlau2 <edlau@ntplx.net>
2020-03-29 14:13:14 -0400

    Add files via upload

--- helpers/Torn-JS-Helpers.js
+++ helpers/Torn-JS-Helpers.js
@@ -1,18 +1,26 @@
 // ==UserScript==
 // @exclude     *
-// @namespace   xedx.helpers
+// @namespace   https://github.com/edlau2
 
 // ==UserLibrary==
 // @name        Torn-JS-Helpers
 // @description Commonly used functions in my Torn scripts.
+// @require     https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
+// @updateURL   https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @version     0.1
 // @license     MIT
 // ==/UserLibrary==
 
 // ==/UserScript==
 
-// @location https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
+initialize();
 
+function initialize() {
+  Number.prototype.format = function (n, x) {
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\.' : '$') + ')';
+    return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
+  };
+}
 
 function validateApiKey() {
     console.log('Helper script validating API key!');
