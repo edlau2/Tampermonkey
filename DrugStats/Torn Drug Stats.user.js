33e46531dc0b3d77a39edfbe5aa3acd0d0e5f758
edlau2 <edlau@ntplx.net>
2020-03-29 16:03:20 -0400

    Start using helper .js

--- DrugStats/Torn Drug Stats.user.js
+++ DrugStats/Torn Drug Stats.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Torn Drug Stats
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Adds drug stats to the home page: drugs used, OD's, Rehabs and rehab total cost to date.
 // @author       xedx [2100735]
 // @include      https://www.torn.com/index.php
 // @updateURL    https://github.com/edlau2/Tampermonkey/blob/master/DrugStats/Torn%20Drug%20Stats.user.js
+// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @require      http://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @connect      tornstats.com
@@ -23,36 +24,13 @@
     'use strict';
 
     //////////////////////////////////////////////////////////////////////
-    // Utility functions
-    //////////////////////////////////////////////////////////////////////
-
-    // HTML constants
-    var CRLF = '<br/>';
-    var TAB = '&emsp;';
-
-    // Check to see if a pointer is valid
-    function validPointer(val, dbg = false) {
-        if (val == 'undefined' || typeof val == 'undefined' || val == null) {
-            if (dbg) {
-                debugger;
-            }
-            return false;
-        }
-        return true;
-    }
-
-    // Insert comma separators into a number
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    }
-
-    //////////////////////////////////////////////////////////////////////
     // Build the Drug Stats div, append underneath the Personal Perks div
     //////////////////////////////////////////////////////////////////////
 
+    var extDivId = 'xedx-drug-stats-ext';
     function buildDrugStatsDiv() {
         // Only do this once
-        var testDiv = document.getElementById('xedx-drug-stats-ext');
+        var testDiv = document.getElementById(extDivId);
         if (validPointer(testDiv)) {
             return;
         }
@@ -63,7 +41,7 @@
         }
 
         // Piece everything together.
-        var extDiv = createExtendedDiv();
+        var extDiv = createExtendedDiv('sortable-box t-blue-cont h', extDivId);
         var hdrDiv = createHeaderDiv();
         var bodyDiv = createBodyDiv();
         var contentDiv = createContentDiv(); // This call also queries the Torn API...
@@ -86,28 +64,6 @@
         var sepHr = document.createElement('hr');
         sepHr.className = 'delimiter-999 m-top10 m-bottom10';
         return sepHr;
-    }
-
-    function createExtendedDiv() {
-        var extendedDiv = document.createElement('div');
-        extendedDiv.className = 'sortable-box t-blue-cont h right';
-        extendedDiv.id = 'xedx-drug-stats-ext';
-        return extendedDiv;
-    }
-
-    function createHeaderDiv() {
-        var headerDiv = document.createElement('div');
-        headerDiv.id = 'xedx-header_div';
-        headerDiv.className = 'title main-title title-black active top-round';
-        headerDiv.setAttribute('role', 'heading');
-        headerDiv.setAttribute('aria-level', '5');
-
-        var arrowDiv = createArrowDiv();
-        var moveDiv = createMoveDiv();
-        headerDiv.appendChild(arrowDiv);
-        headerDiv.appendChild(moveDiv);
-
-        return headerDiv;
     }
 
     function createBodyDiv() {
@@ -190,18 +146,6 @@
         return contentDiv;
     }
 
-    function createDividerSpan(item, name) {
-        var dividerSpan = document.createElement('span');
-        dividerSpan.className = ('divider');
-        dividerSpan.id = 'xedx-div-span-' + item;
-
-        var nameSpan = document.createElement('span');
-        nameSpan.innerText = name;
-        dividerSpan.appendChild(nameSpan);
-
-        return dividerSpan;
-    }
-
     function createValueSpan(item) {
         var value = "0";
         value = queryPersonalStats(item);
@@ -214,24 +158,6 @@
         valSpan.innerText = value;
 
         return valSpan;
-    }
-
-    function createArrowDiv() {
-        var arrowDiv = document.createElement('div');
-        arrowDiv.className = 'arrow-wrap';
-        var a = document.createElement('i');
-        a.className = 'accordion-header-arrow right';
-        arrowDiv.appendChild(a);
-        return arrowDiv;
-    }
-
-    function createMoveDiv() {
-        var moveDiv = document.createElement('div');
-        moveDiv.className = 'move-wrap';
-        var b = document.createElement('i');
-        b.className = 'accordion-header-move right';
-        moveDiv.appendChild(b);
-        return moveDiv;
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -293,7 +219,7 @@
 
         // If this fails, have never used this drug. Not an error.
         if (!validPointer(stats[name])) {
-            console.log('Torn Drug Stats, personalStatsQueryCB, name: ' + name + ' value = 0 (not found)');
+            //console.log('Torn Drug Stats, personalStatsQueryCB, name: ' + name + ' value = 0 (not found)');
             if (!expectedResponses) {
                 addToolTips();
             }
@@ -305,7 +231,6 @@
         } else {
             valSpan.innerText = stats[name];
         }
-        //console.log('Torn Drug Stats, personalStatsQueryCB, name: ' + name + ' value = ' + valSpan.innerText);
 
         // Once we have valid 'drugsused' and 'overdosed', we can calc the percentage.
         if (!name.localeCompare('drugsused') || !name.localeCompare('overdosed')) {
@@ -416,9 +341,6 @@
         }
 
         displayToolTip(useDiv, text);
-
-        //$(useDiv).attr("data-html", "true");
-        //$(useDiv).attr("title", text);
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -457,24 +379,21 @@
         var jsonResp = JSON.parse(responseText);
 
         if (jsonResp.error) {
-            console.log('Torn Drug Stats: re-enabling observer');
             observer.observe(targetNode, config);
             return handleError(responseText);
         }
 
-        console.log('Torn Drug Stats, realBasicQueryCB (travelling check)');
+        console.log('Torn Drug Stats, realBasicQueryCB (travel check)');
 
         var stats = jsonResp.travel;
         console.log('  Destination: ' + stats.destination +
                     '\n  time_left: ' + stats.time_left + ' seconds.');
         if (stats.time_left == 0 && stats.destination == 'Torn') {
-            console.log('Torn Jail Stats: re-enabling observer');
             observer.observe(targetNode, config);
         } else {
             // If travelling, set timeout to re-connect the observer
             // when we are scheduled to land.
             setTimeout(function(){
-                console.log('Torn Jail Stats: re-enabling observer');
                 observer.observe(targetNode, config); },
                        stats.time_left * 1000);
         }
@@ -514,23 +433,13 @@
     //////////////////////////////////////////////////////////////////////
 
     console.log("Torn Drug Stats script started!");
-    var api_key = GM_getValue('gm_api_key');
-    if (api_key == null || api_key == 'undefined' || typeof api_key === 'undefined' || api_key == '') {
-        api_key = prompt("Please enter your API key.\n" +
-                         "Your key will be saved locally so you won't have to be asked again.\n" +
-                         "Your key is kept private and not shared with anyone.", "");
-        GM_setValue('gm_api_key', api_key);
-    }
+    validateApiKey();
 
     var targetNode = document.getElementById('mainContainer');
     var config = { attributes: false, childList: true, subtree: true };
     var callback = function(mutationsList, observer) {
-
-        // debugger;
-
         // Disconnect the observer to prevent looping before
         // we start modifying the page.
-        console.log('Torn Jail Stats: disconnecting observer');
         observer.disconnect();
         buildDrugStatsDiv();
 
@@ -538,10 +447,8 @@
         // or else sets a timeout to re-connect when we land, if
         // we are travelling.
         checkTravelling();
-
-        //observer.observe(targetNode, config);
     };
     var observer = new MutationObserver(callback);
-    console.log('Torn Drug Stats: starting observer');
+
     observer.observe(targetNode, config);
 })();
