d026804f7c8764893906f631b83860748b707012
edlau2 <edlau@ntplx.net>
2020-04-17 13:59:15 -0400

    Version 0.3: generically trap *all* sub-crimes

--- RedRabbit/Red-Rabbit's Test Script.user.js
+++ RedRabbit/Red-Rabbit's Test Script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red-Rabbit's Test Script
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  I am the world!
 // @include      https://www.torn.com/crimes.php*
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
@@ -18,28 +18,39 @@
 (function() {
     'use strict';
 
-
     // This is called from the observer set up below.
+    //
+    // Save 'crimePage' and 'crimeSubPage' text
+    let crimePage = '';
+    let crimeSubPage = '';
     function doSomething() {
         // GM_info is a Tampermonkey variable that has all the data in the ==UserScript== section
         // You don't need to @grant access to it, as you do for GM_get/setValue
         console.log(GM_info.script.name + ' noticed a change!');
 
         // See if our node of interest has loaded yet. If not, we'll be recalled later.
+        // May also be used as an indicator that we couldn't do a crime? Not enough nerve?
         let useNode = document.getElementsByClassName('specials-cont-wrap')[0];
-        if (!validPointer(useNode)) {return;}
+        if (!validPointer(useNode)) {
+            //alert('No special content wrapper!');
+            return; // Will be called back...
+        }
 
         // On the Crimes page, the crimes are in a <ul> (Unordered List) element.
         // Each crime is it's own <li> (List Item). Get the UL. Note that we can access
         // via the var 'useNode from here, the parent DIV.
         let ul = useNode.getElementsByClassName('specials-cont')[0]; // Again, by class name. 'let' instead of 'var' keeps the scope local
+        let errorMsg = null; // Will set and check later
+        let successMsg = null; // Will set and check later
         if (validPointer(ul)) { // Make sure we found it!
             console.log(GM_info.script.name + 'Found the crimes UL!');
 
             // We know the ID's for Kidnap and Mayor, so just search for those.
             // We could use these just to determine where we are on all the 'crimes' pages.
-            let kidnapBtn = document.getElementById('kidnapping');
-            let mayorBtn = document.getElementById('mayor');
+            let kidnapBtn = document.getElementById('kidnapping'); // Indicates we're on the main crimes page
+
+            //let mayorBtn = document.getElementById('mayor');       // Indicates we're on the 'kidnapping' sub-page
+            //let heavyMetalBtn = document.getElementById('heavy-metal-cds'); // Indicates we're on the 'Sell copied Media' sub-page
 
             //
             // This means we are on the page where we select what crime, in general.
@@ -52,30 +63,63 @@
             // we really want, which is this element:
             // <input id="kidnapping" class="radio-css without-label" type="radio" value="kidnapping" name="crime">
             // but that is not working, for some reason I tried about 10 different ways. With no success.
+            // Normally, we'd get the button using getELementById(), as above - so kidnapBtn, for example,
+            // refers tot he 'kidnapping' crimes button, and use something like 'kidnapBtn.click(function(){ ..do something});'
+            // or kidnapButton.addEventListener('click', function() {...do something...});
             //
-            if (validPointer(kidnapBtn)) {
-                alert('Found the "kidnap" button!');
-                $("label").click(function (e) {
-                    alert('clicked on "' + e.currentTarget.innerText + '"');
-                    console.log('Clicked on "' + e.currentTarget.innerText + '"', e);
+            if (validPointer(kidnapBtn)) { // Indicates primary crimes page.
+                alert(/*'Found the "kidnap" button!*/ 'On Primary crime page!');
+                $("label").click(function (e) { // Sets up a handler for all the labels on the current page
+                    // These lines could be replaced with a separate fn, say 'handleCrimePage(...)'
+                    alert('clicked on "' + e.currentTarget.innerText + '" label');
+                    console.log('Clicked on "' + e.currentTarget.innerText + '" label', e);
+                    crimePage = e.currentTarget.innerText;
+                    crimeSubPage = '';
                 });
             }
 
             //
-            // This means we're on the next page, the 'sub crime'. Here, if the
-            // mayorBtn is valid, we're on the Kidnapping page, it was pressed.
-            // We got the button, above: "let mayorBtn = document.getElementById('mayor');"
+            // Regardless of finding those specific buttons, if we wanted to track all crimes,
+            // could probably set on *any* label, here - don't bother being specific ?
             //
-            // Again, I set a fn to be called on ALL labels, we can differentiate by name.
-            // The name is 'e.currentTarget.innerText', where 'e' is the event that triggered
-            // this, specifically the 'click' event.
-            if (validPointer(mayorBtn)) {
-                alert('Found the "mayor" button!');
-                $("label").click(function (e) {
-                     alert('clicked on "'+ e.currentTarget.innerText + '"');
-                     console.log('Clicked on "' + e.currentTarget.innerText + '"', e);
-                });
-            }
+            // Or, do a loop, based on an array - test this out!
+            //
+            let subCrimeArray = ['search-the-train-station', // Search for cash
+                                  'heavy-metal-cds',          // Sell copied media
+                                  'sweet-shop',               // Shoplift
+                                  'hobo',                     // Pickpocket
+                                  'apartment',                // Larceny
+                                  'swift-robbery',            // Armed robbery
+                                  'sell-pills',               // Transport drugs
+                                  'simple-virus',             // Plant a computer virus
+                                  'car-bomb',                 // Assasination
+                                  'warehouse',                // Arson
+                                  'hijack-a-car',             // Grand theft auto
+                                  'side-door',                // Pawn shop
+                                  'money',                    // Counterfeiting
+                                  'mayor',                    // Kidnapping
+                                  'firearms',                 // Arms trafficking
+                                  'bomb-a-factory',           // Bombings
+                                  'hack-into-a-bank-mainframe', // Hacking
+                                 ];
+            subCrimeArray.forEach(function(crime) {
+                let subCrime = document.getElementById(crime);
+                if (validPointer(subCrime)) { // THis indicates we are on a crime's sub-page
+                    // These lines could be replaced with a separate fn, say 'handleSubCrimePage(...)'
+                    alert('On the "' + crimePage + '" crimes sub-page!');
+                    $("label").click(function (e) {  // Sets up a handler for all the labels on the current page
+                        crimeSubPage = e.currentTarget.innerText;
+                        alert('clicked on "'+ crimePage + '-->' + crimeSubPage + '" label');
+                        console.log('Clicked on "' + crimePage + '-->' + crimeSubPage + '" label', e);
+                     });
+                }
+            });
+
+
+        } else if (validPointer(errorMsg = useNode.getElementsByClassName('error-message')[0])) { // Look for an error result of a crime
+            alert('Detected crime error: ' + errorMsg.innerText);
+        } else if (validPointer(successMsg = useNode.getElementsByClassName('success-message')[0])){
+            alert('Detected crime success: ' + successMsg.innerText);
         }
 
     console.log(GM_info.script.name + 'All done with "doSomething"!');
@@ -119,7 +163,7 @@
     //     doSomething();
     // });
     //
-    // -or-
+    // THis fires on initial page load
     window.onload = function () {
         console.log(GM_info.script.name + ' onLoad');
         doSomething();
@@ -137,9 +181,6 @@
 
         observer.observe(targetNode, config); // Re-start the observer
     };
-
-    //doSomething(); // I call here also, since we want to do some stuff *before* a change is made also,
-                   // if already loaded. We don't need this -and- the onLoad handler...
 
     var observer = new MutationObserver(callback); // Create the observer
     observer.observe(targetNode, config); // Start the observer
