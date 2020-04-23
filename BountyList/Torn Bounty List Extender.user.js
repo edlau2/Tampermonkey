a7ffffb00df196a368d59d63e091224659910c47
edlau2 <edlau@ntplx.net>
2020-04-22 23:55:13 -0400

    Cleanup

--- BountyList/Torn Bounty List Extender.user.js
+++ BountyList/Torn Bounty List Extender.user.js
@@ -1,14 +1,12 @@
 // ==UserScript==
 // @name         Torn Bounty List Extender
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Add rank to bounty list display
 // @author       xedx [2100735]
 // @include      https://www.torn.com/bounties.php*
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @updateURL    https://github.com/edlau2/Tampermonkey/raw/master/BountyList/Torn%20Bounty%20List%20Extender.user.js
-// @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @require      http://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @connect      api.torn.com
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
@@ -22,62 +20,41 @@
     // Global cache of ID->Rank associations
     var rank_cache = ['', 0];
 
-    //////////////////////////////////////////////////////////////////////
     // Query profile information based on ID
-    //////////////////////////////////////////////////////////////////////
-
     function getRankFromId(ID, li) {
         console.log("Querying Torn for rank, ID = " + ID);
         xedx_TornUserQuery(ID, 'profile', updateUserLevelsCB, li);
     }
-
-    //////////////////////////////////////////////////////////////////////
-    // This callback create a cache used to store ID-rank associations
-    // and updates the actual <li> in the <ul>.
-    //////////////////////////////////////////////////////////////////////
 
     function updateUserLevelsCB(responseText, ID, li) {
         var jsonResp = JSON.parse(responseText);
         if (jsonResp.error) {return handleError(responseText);}
 
         let numeric_rank = numericRankFromFullRank(jsonResp.rank);
-        console.log("Caching rank: " + ID + " ==> " + numeric_rank);
+        //console.log("Caching rank: " + ID + " ==> " + numeric_rank);
         rank_cache.push({ID, numeric_rank});
         updateLiWithRank(li, numeric_rank);
     }
 
+    // Write to the UI
     function updateLiWithRank(li, numeric_rank) {
         observer.disconnect();
-
-        let dataId = li.getAttribute('data-id');
         let lvlNode = li.getElementsByClassName('level')[0];
         let text = lvlNode.innerText;
-        let html = lvlNode.innerHTML;
-
-        if (text.indexOf("/") != -1) {
+        if (text.indexOf("/") != -1) { // Don't do again!
             observer.observe(targetNode, config);
             return;
         }
 
-        // Don't need to update both here ....
         lvlNode.innerText = text.trim() + '/' + (numeric_rank ? numeric_rank : '?');
-        lvlNode.innerHTML = html.trim() + '/' + (numeric_rank ? numeric_rank : '?');
-
-        let text2 = lvlNode.innerText;
-        let html2 = lvlNode.innerHTML;
-
-        //console.log("Updated level text (data-id = " + dataId + ") to '" + lvlNode.innerText + "'");
         observer.observe(targetNode, config);
     }
 
-    //////////////////////////////////////////////////////////////////////
     // Find a rank from our cache, based on ID
-    //////////////////////////////////////////////////////////////////////
-
     function getCachedRankFromId(ID, li) {
         for (var i = 0; i < rank_cache.length; i++) {
             if (rank_cache[i].ID == ID) {
-                console.log("Returning cached rank: " + ID + " ==> " + rank_cache[i].numeric_rank);
+                //console.log("Returning cached rank: " + ID + " ==> " + rank_cache[i].numeric_rank);
                 updateLiWithRank(li, rank_cache[i].numeric_rank);
                 return rank_cache[i].numeric_rank;
             }
@@ -87,7 +64,7 @@
 
     //////////////////////////////////////////////////////////////////////
     // This prepares to update the UI by locating level, user ID
-    // and the index of the <li> in the <ul>.
+    // and getting the rank for each of the <li>'s in the <ul>.
     //////////////////////////////////////////////////////////////////////
 
     function updateUserLevels() {
@@ -105,9 +82,10 @@
                            .children[0] // li.b-info-wrap.head
                            .children[1] // div.target.left
                            .children[0]; // a
+            // let idNode = $(li).find('ul.item > li.b-info-wrap.head > div.target.left > a);
 
             let ID = idNode.getAttribute('href').split('=')[1];
-            console.log('Locating rank for ID ' + ID);
+            //console.log('Locating rank for ID ' + ID);
             if (!getCachedRankFromId(ID, li)) {
                 getRankFromId(ID, li);
             }
