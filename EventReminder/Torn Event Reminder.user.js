baaa4274de6f0b92cd416952ca073ec8e964aa46
edlau2 <edlau@ntplx.net>
2020-05-01 00:51:30 -0400

    Add handlers for adding new events.

--- EventReminder/Torn Event Reminder.user.js
+++ EventReminder/Torn Event Reminder.user.js
@@ -11,6 +11,7 @@
 // replace these with URL's when done testing!!!
 // @require      file:////Users/edlau/Documents/Tampermonkey Scripts/EventReminder/Torn-Event-Reminder-CSS.js
 // @resource     datetimepickerCSS file:////Users/edlau/Documents/JQuery/DateTimePicker/jquery.datetimepicker.min.css
+// @require      file:////Users/edlau/Documents/JQuery/DateTimePicker/jquery.datetimepicker.full.min.js
 //
 // @grant        GM_addStyle
 // @grant        GM_getValue
@@ -27,9 +28,9 @@
 const selectBtnSpan =
       '<select name="xedx-select" class="custom-select select-selected" id="xedx-select">' +
               // The width will auto-expand to the widest event. How about to current?
-              '<option value="next">Upcoming...</option>' +
-              '<option value="second">Next Event really long wut wut wut?</option>' +
-              '<option value="third">Event 3</option>' +
+              '<option value="0">Upcoming...</option>' +
+              '<option value="1">Next Event really long wut wut wut?</option>' +
+              '<option value="2">Event 3</option>' +
           '</select>';
 
 const add_event_div =
@@ -48,7 +49,16 @@ const add_event_div =
 // The datetimepicker is a plugin, and is @resource'd in; it is
 // distributed as a .css file so requires that the resource be
 // loaded via GM_getResourceText (...) and GM_addStyle(...)
+// See: https://xdsoft.net/jqplugins/datetimepicker/
 
+
+function autoSizeSelect(elem) {
+    var text = $(elem).find('option:selected').text()
+    var $aux = $('<select/>').append($('<option/>').text(text))
+    $(elem).after($aux)
+    $(elem).width($aux.width())
+    $aux.remove()
+}
 
 (function() {
     'use strict';
@@ -82,10 +92,19 @@ const add_event_div =
     $(parent).append(add_event_div);
     const target = document.getElementById('xedx-events');
 
+    // Build empty div for 'Add Event' dialog.
+    let configDiv = document.getElementById('add-event-dlg');
+    if (!validPointer(configDiv)) { // Only do once!
+        insertAddEventDialogDiv();
+    }
+
+    // ****** UI is now available, can configure now ******
+
     // Add fake button handler, for the "Add Event" button,
     // which isn't really a button. But who cares.
     $("#xedx-add-event").click(function(){
-        alert('I got a click');
+        //alert('Adding a new event...');
+        launchAddEventDlg();
     });
 
     // Start the clock ticking...
@@ -96,11 +115,14 @@ const add_event_div =
 
     // Dynamically resize the select
     $('.custom-select').change(function(){
+        autoSizeSelect(this);
+        /*
         var text = $(this).find('option:selected').text()
         var $aux = $('<select/>').append($('<option/>').text(text))
         $(this).after($aux)
         $(this).width($aux.width())
         $aux.remove()
+        */
     }).change()
 
     // Set the selected event to the first one, by index, 0-based.
@@ -117,4 +139,99 @@ const add_event_div =
 
 })();
 
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Dialog options for the 'Options'/'Configure' page - JQuery-UI wrapper around above
+//////////////////////////////////////////////////////////////////////////////////////
+
+// Handler for the 'Add Event' button
+
+function btnOnOkClick() {
+    let count = $('#xedx-select option').length;
+    let newOpt = '<option value="' + count + '">' + $('#datetimepicker').val() + ' : ' + $('#event-name').val() +'</option>';
+    $('#xedx-select').append(newOpt);
+    $('.custom-select').prop('selectedIndex', count);
+    autoSizeSelect($('.custom-select'));
+}
+
+function launchAddEventDlg() {
+    let dlgBase = document.getElementById("add-event-dlg");
+    dlgBase.style.display = "none";
+    var addEventDlg = $("#add-event-dlg").dialog(configOpt);
+    addEventDlg.dialog("open");
+
+    // Further dialog customization (move to CSS class!)
+    $(".ui-dialog").attr("style", $(".ui-dialog").attr("style") + " border: solid black 2px; border-radius: 5px;background-color: white;");
+    $(".ui-dialog-titlebar-close").hide();
+    $(".ui-dialog-titlebar").attr("style", "text-align: center; margin: 5px; color: black; font-weight: bold;");
+
+    let okBtn = document.getElementById("xedx-btn-ok");
+    okBtn.addEventListener("click", function() {
+        btnOnOkClick();
+        let dlgBase = document.getElementById("add-event-dlg");
+        dlgBase.style.display = "none";
+        $("#add-event-dlg").dialog("close");
+    }, false);
+}
+
+function insertAddEventDialogDiv() {
+
+    // Main body of the dialog style - note
+    // that the BG color of what s behind it bleeds through.
+    const addEventStyle = '.event-style {' +
+      'display: none; text-align: center;' +
+      'border: solid black 2px;' +
+      'border-radius: 0px;' +
+      'background-color: #000000;' +
+      'filter: alpha(opacity=80);' +
+      'opacity: 0.80;' +
+      'font-weight: bold;' +
+      'font-size: 12px;' +
+      'color: #fff;' +
+      '}';
+
+    GM_addStyle(addEventStyle);
+    $(".body").append(
+          '<div id="add-event-dlg" class="event-style">' +
+              '<br><br><br>' +
+              '<p>' +
+                  '<label for="event-name">Event name: </label>' +
+                  '<input type="text" id="event-name" name="event-name">New Event</input><br><br>' +
+                  '<label for="datetimepicker">Select time: </label>' +
+                  '<input id="datetimepicker" type="text"></input>' +
+              '</p>' +
+          '</div>');
+
+    $('#datetimepicker').datetimepicker( // Initialization
+        {theme:'dark',
+         format:'d/m/Y h:i',
+        }
+    );
+}
+
+// Define options for the 'Options' dialog
+var configOpt =
+    {
+        //draggable: true,
+        resizable: true,
+        autoOpen: false,
+        modal: true,
+        title: "Add Event",
+        buttons: [
+            {
+                text: "Ok",
+                id: "xedx-btn-ok",
+            },
+            {
+                text: "Cancel",
+                id: "xedx-btn-close",
+                click: function() {
+                    let cfgBase = document.getElementById("add-event-dlg");
+                    cfgBase.style.display = "block";
+                    $(this).dialog( "close" );
+                },
+            },
+        ]
+    };
 
