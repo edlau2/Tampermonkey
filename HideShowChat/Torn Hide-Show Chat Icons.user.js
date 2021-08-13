4e65de04985d22913ccc5d5df1ca077a9af54180
edlau2 <edlau@ntplx.net>
2021-08-13 18:41:56 -0400

    Delay load until DOM complete
    This hopefully lets other scripts, namely NPC Loot Timers, load first.

--- HideShowChat/Torn Hide-Show Chat Icons.user.js
+++ HideShowChat/Torn Hide-Show Chat Icons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Hide-Show Chat Icons
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Toggles the display of chat icons at the bottom of the screen
 // @author       xedx [2100735]
 // @include      https://www.torn.com/*
@@ -18,7 +18,7 @@
 
     logScriptStart();
 
-    var hideChatDiv = '<hr class="delimiter___neME6"><div id="xedxShowHideChat" style="padding-bottom: 5px"><span style="font-weight: 700;">Chat Icons</span>' +
+    var hideChatDiv = '<hr class="delimiter___neME6"><div id="xedxShowHideChat" style="padding-bottom: 5px; padding-top: 5px;"><span style="font-weight: 700;">Chat Icons</span>' +
         '<a id="showHideChat" class="t-blue show-hide">[hide]</a>';
 
     function hideChat(hide) {
@@ -27,10 +27,19 @@
         document.querySelector("#chatRoot > div").style.display = hide ? 'none' : 'block';
     }
 
-    $('#sidebar').find('div[class^=toggle-content__]').find('div[class^=content___]').append(hideChatDiv);
-    $('#showHideChat').on('click', function () {
+    function handlePageLoaded() {
+        $('#sidebar').find('div[class^=toggle-content__]').find('div[class^=content___]').append(hideChatDiv);
+        $('#showHideChat').on('click', function () {
             const hide = $('#showHideChat').text() == '[hide]';
             hideChat(hide);
         });
+    }
+
+    // Delay until DOM content load (not full page) complete, so that other scripts run first.
+    if (document.readyState == 'loading') {
+        document.addEventListener('DOMContentLoaded', handlePageLoaded);
+    } else {
+        handlePageLoaded();
+    }
 
 })();
