92d4609d22cf5f7be2dbefdc4b47012c0bed0b48
edlau2 <edlau@ntplx.net>
2021-11-22 23:24:58 -0500

    Add options to select any lottery

--- DailyDime/Torn One-Click Daily Dime.user.js
+++ DailyDime/Torn One-Click Daily Dime.user.js
@@ -1,30 +1,38 @@
 // ==UserScript==
 // @name         Torn One-Click Daily Dime
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Clicks on the daily dime 'X' times for you with one click
+// @version      0.4
+// @description  Clicks on a selected lottery 'X' times for you with one click
 // @author       xedx [2100735]
 // @include      https://www.torn.com/loader.php?sid=lottery
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_addStyle
 // @grant        unsafeWindow
 // ==/UserScript==
 
-// This is just easier to read this way, instead of one line.
-// Could also have be @required from a separate .js....
 const daily_dime_div =
       '<div class="t-blue-cont h" id="xedx-dailydime-ext">' +
-          '<div id="xedx-header_div" class="title main-title title-black active top-round" role="heading" aria-level="5">One-Click Daily Dime</div>' +
-          '<div id="xedx-content-div" class="cont-gray bottom-round" style="height: 56px; overflow: auto">' +
+          '<div id="xedx-header_div" class="title main-title title-black active top-round" role="heading" aria-level="5">One-Click Lottery</div>' +
+          '<div id="xedx-content-div" class="cont-gray bottom-round" style="height: auto; overflow: auto">' +
               '<div style="text-align: center">' +
-                  '<span>' +
+                  '<span class="xedx-main">' +
                       //'<button id="xedx-buy-btn" style="font-size: 14px; height: 24px; text-align: center;border-radius: 5px; margin: 15px 40px; ' +
                       //'background: LightGrey; border: 1px solid black;">Buy</button>' +
-                      '<button id="xedx-buy-btn" class="btn-dark-bg">Buy</button>' +
+                      '<button id="xedx-buy-btn" class="btn-dark-bg">Click to Buy</button>' +
                       '<input id="xedx-slot-turns" type="number" style="font-size: 14px; height: 24px; text-align: center;' +
                       'border-radius: 5px; margin: 15px 40px; border: 1px solid black;">' +
-                      '<B>Daily Dime tickets</B>' +
+                      '<B>Lottery Tickets</B>' +
+                      '<p>Select a Lottery:</p>' +
+                      '<div>' +
+                          '<input type="radio" id="DD" class="xedx-oneclick" name="lottery" data="daily-dime" value="DD" checked>' +
+                          '<label for="DD"> Daily Dime </label>' +
+                          '<input type="radio" id="LSC" class="xedx-oneclick" name="lottery" data="lucky-shot" value="LSC">' +
+                          '<label for="LSC">Lucky Shot Casino</label>' +
+                          '<input type="radio" id="HG" class="xedx-oneclick" name="lottery" data="holy-grail" value="HG">' +
+                          '<label for="HG">Holy Grail</label>'+
+                      '</div>'+
                   '</span>' +
               '</div>' +
           '</div>' +
@@ -37,26 +45,40 @@ const daily_dime_div =
     var buyBtn = null; // Lotto buy button
     var inputField = null;
     var slotturnsDiv = null;
+    var selectedLottery = 'daily-dime';
+
+    GM_addStyle(".xedx-oneclick {margin: 10px;}");
+    GM_addStyle(".xedx-main {margin-left: 0px;}");
+
 
     function createMainDiv() {
         if (validPointer(document.getElementById('xedx-dailydime-ext'))) {return;} // Only do once
-        var parentDiv = document.getElementsByClassName("lottery-wrap"); // $('#lottery-wrap')
+        var parentDiv = document.getElementsByClassName("lottery-wrap");
         $(parentDiv).append(separator);
         $(parentDiv).append(daily_dime_div);
 
-        // Could change this to pick any of the lottos...
-        // 'daily-dime', 'lucky-shot', or 'holy-grail'
-        lottoDiv = document.getElementById('daily-dime');
+        lottoDiv = document.getElementById(selectedLottery);
         buyBtn = lottoDiv.getElementsByClassName('btn')[0];
         inputField = document.getElementById('xedx-slot-turns');
         slotturnsDiv = document.getElementsByClassName('slotturns')[0];
 
-        let myBuyButton = document.getElementById('xedx-buy-btn');
-        myBuyButton.addEventListener('click',function () {
+        document.getElementById('xedx-buy-btn').addEventListener('click',function () {
             buyFunction();
         });
 
+        document.getElementById("DD").addEventListener('click', function (event) {
+            handleRadioBtn(event.target)}, { passive: false });
+        document.getElementById("HG").addEventListener('click', function (event) {
+            handleRadioBtn(event.target)}, { passive: false });
+        document.getElementById("LSC").addEventListener('click', function (event) {
+            handleRadioBtn(event.target)}, { passive: false });
+
         refresh();
+    }
+
+    function handleRadioBtn(target) {
+        selectedLottery = target.getAttribute('data');
+        console.log('Lottery Select:', target, ' selectedLottery: ', selectedLottery);
     }
 
     //////////////////////////////////////////////////////////////////////
@@ -64,26 +86,28 @@ const daily_dime_div =
     //////////////////////////////////////////////////////////////////////
 
     function buyFunction() {
-        refresh();
+        //refresh();
         let turns = parseInt(inputField.value);
         buyBtn.disabled = true;
 
         let maxTurns = slotturnsDiv.innerText;
         if (turns > maxTurns) {inputField.value = turns = maxTurns;}
 
-        console.log("Daily Dime debug: Will buy " + turns + " times.");
-        for (let i = 0; i < turns; i++) {
+        log("Will buy " + turns + " times.");
+        log('Turns: ' + turns + ' Max turns: ' + maxTurns);
+        for (let i = turns; i > 0; i--) {
             buyBtn.click();
             maxTurns--;
+            turns--;
         }
         refresh(maxTurns);
     }
 
-    function refresh(maxTurns = -1) {
-        if (maxTurns == -1) {
+    function refresh(value = -1) {
+        if (value == -1) {
             inputField.value = slotturnsDiv.innerText;
         } else {
-            inputField.value = maxTurns;
+            inputField.value = value;
         }
 
         buyBtn.disabled = (inputField.value == 0);
@@ -96,6 +120,7 @@ const daily_dime_div =
     //////////////////////////////////////////////////////////////////////
 
     logScriptStart();
+    versionCheck();
 
     var targetNode = document.getElementById('mainContainer');
     var config = { attributes: false, childList: true, subtree: true };
