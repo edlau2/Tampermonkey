f3b9b1c80276d9958cea94d51dffecdb6db6e2c2
edlau2 <edlau@ntplx.net>
2021-12-18 17:46:58 -0500

    Create See The Temps.user.js

---
+++ Third Party/SeeTheTemps/See The Temps.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         See the temps
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       _syntaxera_
+// @match        https://www.torn.com/loader.php?sid=attack*
+// @icon         https://www.google.com/s2/favicons?domain=torn.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle ( `
+        .defender___2q-P6 {
+            background:none !important;
+        }
+    }` );
+})();
