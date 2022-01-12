2542aa392147302550b15b846ad78dbed67b7d9c
edlau2 <edlau@ntplx.net>
2022-01-12 16:10:49 -0500

    Now can save to the clipboard

--- SecretSaver/Torn Secret Saver.user.js
+++ SecretSaver/Torn Secret Saver.user.js
@@ -3,7 +3,67 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.4
 // @description  Detects when your 'secret' changes.
+// @author       xedx [2100735]// ==UserScript==
+// @name         Torn Secret Saver
+// @namespace    http://tampermonkey.net/
+// @version      0.4
+// @description  Detects when your 'secret' changes.
 // @author       xedx [2100735]
+// @include      https://www.torn.com/*
+// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //////////////////////////////////////////////////////////////////////
+    // Main.
+    //////////////////////////////////////////////////////////////////////
+
+    logScriptStart();
+    versionCheck();
+
+    function timenow() {
+        return new Date().toLocaleString();
+    }
+
+    // The timeout is required so the doc window has time to regain focus.
+    function updateClipboard(newClip) {
+        setTimeout(function() {navigator.clipboard.writeText(newClip).then(function() {
+            alert('Your new secret has been saved to the clipboard!');
+        }, function(e) {
+            alert('Error: ', e);
+        })}, 1000);
+    }
+
+    const savedSecret = GM_getValue('secret'); //"64ae61cdf04d3ba3d3b3b2ddc5e853d099f89a53b3ea2f8228674a730ea40263";
+    var secret = $('script[secret]').attr("secret");
+    if (savedSecret == undefined || !savedSecret) {
+        GM_setValue('secret' + secret);
+        GM_setValue('nowTimestamp', timenow());
+    }
+    if (secret != savedSecret && savedSecret) {
+        GM_setValue('secret', secret);
+        GM_setValue('oldTimestamp', GM_getValue('nowTimestamp'));
+        GM_setValue('nowTimestamp', timenow());
+        let text = GM_info.script.name + ' Secret has changed!\n' +
+              'Old: ' + savedSecret + '\n' +
+              'New: ' + secret + '\n\nSave to the clipboard?';
+        if (confirm(text)) updateClipboard(secret);
+
+        /*
+        alert(GM_info.script.name + ' Secret has changed!\n' +
+              'Old: ' + savedSecret + '\n' +
+              'New: ' + secret);
+        */
+    }
+
+})();
 // @include      https://www.torn.com/index*
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @grant        GM_addStyle
