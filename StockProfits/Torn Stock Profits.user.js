6384fc680f48103b9ddf2825fc172104b89a2d0b
edlau2 <edlau@ntplx.net>
2022-02-06 22:05:28 -0500

    Fix for crash if stock column expanded (no ID)

--- StockProfits/Torn Stock Profits.user.js
+++ StockProfits/Torn Stock Profits.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Stock Profits
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.4
 // @description  Displays current stock profits for owned shares
 // @author       xedx [2100735]
 // @include      https://www.torn.com/page.php?sid=stocks*
@@ -25,13 +25,13 @@
 
     // Callbacks for our Torn API calls.
     function userStocksCB(responseText, ID, param) {
-        console.log(GM_info.script.name + ' userStocksCB');
+        log(GM_info.script.name + ' userStocksCB');
         userStocksJSON = JSON.parse(responseText);
         if (userStocksJSON.error) {return handleError(responseText);}
     }
 
     function tornStocksCB(responseText, ID, param) {
-        console.log(GM_info.script.name + ' userStocksCB');
+        log(GM_info.script.name + ' tornStocksCB');
         tornStocksJSON = JSON.parse(responseText);
         if (tornStocksJSON.error) {return handleError(responseText);}
 
@@ -52,28 +52,29 @@
         var stocksList = mainStocksUL.getElementsByTagName('ul');
         if (stocksList.length < 2) return setTimeout(handlePageLoaded, 1000); // Check should not be needed
 
-        console.log('userStocksJSON: ', userStocksJSON);
-        console.log('tornStocksJSON: ', tornStocksJSON);
+        //console.log('userStocksJSON: ', userStocksJSON);
+        //console.log('tornStocksJSON: ', tornStocksJSON);
         if (!userStocksJSON || !tornStocksJSON) return setTimeout(handlePageLoaded, 1000);
 
         // -- Now we're all good to go. --
         for (let i = 0; i < stocksList.length; i++) {
             let stockUL = stocksList[i];
             let stockID = stockUL.id;
+            if (!stockID) continue; // Expanded stock column
             let stockName = stockNameFromID(stockID);
             let stockPrice = stockPriceFromID(stockID);
-            log('Stock ' + i + ' ID = ' + stockID);
+            //log('Stock ' + i + ' ID = ' + stockID);
             if (!userStocksJSON.stocks[stockID]) { // Un-owned
-                log("Don't own stock ID " + stockID + ', ' + stockName);
+                //log("Don't own stock ID " + stockID + ', ' + stockName);
                 continue;
             }
-            log('Editing stock ' + name);
+            //log('Editing stock ' + name);
             stockUL.setAttribute('style', 'height: auto;');
             let owned = userStocksJSON.stocks[stockID].transactions;
             let keys = Object.keys(owned);
             for (let j = 0; j < keys.length; j++) {
                 let ownedLI = stockUL.querySelector("#ownedTab");
-                console.log('ownedLI: ', ownedLI);
+                //console.log('ownedLI: ', ownedLI);
                 let ownedShares = owned[keys[j]].shares;
                 let boughtPrice = owned[keys[j]].bought_price;
                 let profit = (stockPrice - boughtPrice) * ownedShares; // Gross profit
