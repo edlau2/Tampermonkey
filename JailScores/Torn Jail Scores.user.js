9b831eed2a4c7f096fe4bcd1fe87caed8aa08fc1
edlau2 <edlau@ntplx.net>
2022-04-08 13:12:46 -0400

    Initial update for improved success rate estimates

--- JailScores/Torn Jail Scores.user.js
+++ JailScores/Torn Jail Scores.user.js
@@ -1,18 +1,38 @@
 // ==UserScript==
 // @name         Torn Jail Scores
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Add 'score' to jailed people list
+// @version      0.3
+// @description  Add 'difficulty' to jailed people list
 // @author       xedx [2100735]
 // @include      https://www.torn.com/jailview.php*
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
+// @connect      api.torn.com
+// @connect      www.tornstats.com
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
 // @grant        unsafeWindow
 // ==/UserScript==
 
+/*eslint no-unused-vars: 0*/
+/*eslint no-undef: 0*/
+/*eslint no-multi-spaces: 0*/
+
+// The formulas used in here are taken from this forum post:
+// https://www.torn.com/forums.php#/p=threads&f=61&t=16192039&b=0&a=0
+
 (function($) {
     'use strict';
+
+    debugLoggingEnabled = false;
+    loggingEnabled = true;
+
+    // Global vars
+    var targetNode = null;
+    var observer = null;
+    var perks = {"bustChanceIncrease": 0, "bustSkillIncrease": 0, "lawPerk": false};
+    const config = { attributes: false, childList: true, subtree: true };
 
     /////////////////////////////////////////////////////////////////////////
     // Helper to parse a time string (33h 14m format), converting to minutes
@@ -42,13 +62,11 @@
     //////////////////////////////////////////////////////////////////////
 
     function addJailScores() {
+        log('[addJailScores]');
         // Get the <UL> and list of <li>'s
         var elemList = document.getElementsByClassName('user-info-list-wrap icons users-list bottom-round');
         var items = elemList[0].getElementsByTagName("li");
-
-        // We seem to be called twice, the first call always has a length of 1.
-        // It seems we can ignore this call.
-        if (items.length <= 1) {return;}
+        if (items.length <= 1) {return setTimeout(addJailScores, 250);}
 
         for (var i = 0; i < items.length; ++i) {
             // Get the wrapper around the time and level (and reason)
@@ -60,31 +78,84 @@
             if (lvlStr.indexOf("(") != -1) {return;} // Don't do this more than once!
 
             var minutes = parseJailTimeStr(timeStr);
-            var score = minutes * parseInt(lvlStr);
+            var score = (minutes + 180) * parseInt(lvlStr);
 
-            // Write out the score, append to the level.
+            // Write out the 'difficulty', append to the level.
             var scoreStr = score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             wrapper.children[1].innerText = lvlStr + " (" + scoreStr + ") ";
          }
     }
 
+    // Get data used to calc success chance via the Torn API
+    function personalStatsQuery() {
+        log('Calling xedx_TornUserQuery');
+        xedx_TornUserQuery(null, 'personalstats,perks,basic', personalStatsQueryCB);
+    }
+
+    // Callback for above
+    function personalStatsQueryCB(responseText, ID) {
+        log('[personalStatsQueryCB] ');
+        let jsonResp = JSON.parse(responseText);
+        if (jsonResp.error) {
+            log('Response error!');
+            return handleError(responseText);
+        }
+
+        let personalstats = jsonResp.personalstats;
+        let facPerks = jsonResp.faction_perks;
+        let eduPerks = jsonResp.education_perks;
+        let jobPerks = jsonResp.job_perks;
+        let userLvl = jsonResp.level;
+
+        debug('personalstats: ', personalstats);
+        debug('facPerks: ', facPerks);
+        debug('eduPerks: ', eduPerks);
+        debug('level: ', userLvl);
+
+        // From fac perks, see what (if any) 'bust success' increase there is
+        for (let i=0; i<facPerks.length; i++) {
+            if (facPerks[i].toLowerCase().indexOf('bust success') > -1) {
+                perks.bustChanceIncrease = Number(facPerks[i].match(/(\d+)/)[0].trim());
+                break;
+            }
+        }
+
+        // Same from eduPerks: 'Busting skill'
+        for (let i=0; i<eduPerks.length; i++) {
+            if (eduPerks[i].toLowerCase().indexOf('busting skill') > -1) {
+                perks.bustSkillIncrease = Number(eduPerks[i].match(/(\d+)/)[0].trim());
+                break;
+            }
+        }
+
+        log('Perks: ', perks);
+
+        // Add jail scores to the page, and install an observer to monitor for page changes.
+        addJailScores();
+    }
+
+    // Start a mutation observer to run on page changes (unused)
+    function installObserver() {
+        targetNode = document.getElementById('mainContainer');
+        const callback = function(mutationsList, observer) {
+            observer.disconnect();
+            addJailScores();
+            observer.observe(targetNode, config);
+        };
+        observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+    }
+
     //////////////////////////////////////////////////////////////////////
-    // Main entry point. Start an observer so that we trigger when we
-    // actually get to the page(s) - technically, when the page(s) change.
-    // As they do on load. Seems more reliable than onLoad().
+    // Main entry point. 
     //////////////////////////////////////////////////////////////////////
 
     logScriptStart();
+    validateApiKey();
+    versionCheck();
 
-    var targetNode = document.getElementById('mainContainer');
-    var config = { attributes: false, childList: true, subtree: true };
-    var callback = function(mutationsList, observer) {
-        observer.disconnect();
-        addJailScores();
-        observer.observe(targetNode, config);
-    };
-    var observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
+    // Start by kicking off a few API calls.
+    personalStatsQuery();
 
 })();
 
