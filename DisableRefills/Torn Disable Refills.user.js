6a3de5689dd82af0b09de2586fd8ce787e3a11f1
edlau2 <edlau@ntplx.net>
2022-05-28 20:54:57 -0400

    Deprecated: Moved to TTS

--- DisableRefills/Torn Disable Refills.user.js
+++ DisableRefills/Torn Disable Refills.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Disables refills in the points building if bars aren't empty.
 // @author       xedx [2100735]
-// @include      https://www.torn.com/points.php*
+// @match      https://www.torn.com/points.php*
 // @connect      api.torn.com
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @grant        GM_addStyle
@@ -14,22 +14,25 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
+/*eslint no-unused-vars: 0*/
+/*eslint no-undef: 0*/
+/*eslint no-multi-spaces: 0*/
+
 (function() {
     'use strict';
 
     var safetyOn = true;
     var jsonResp = null;
-    let safetyNet = '<input class="xedx-ctrls" type="checkbox" id="confirm" name="confirm" value="confirm" checked>' +
-                    '<label for="confirm">  Safety Net! Note that clicking the title bar will bypass the safety net.</label>';
+    let safetyNet = '<input class="xedx-ctrls" type="checkbox" id="refill_confirm" name="refill_confirm" value="refill_confirm" checked>' +
+                    '<label for="refill_confirm">  Safety Net! Note that clicking the title bar will bypass the safety net.</label>';
 
     function onCheckboxClicked() {
-        let ckBox = document.querySelector("#confirm");
+        let ckBox = document.querySelector("#refill_confirm");
         safetyOn = ckBox.checked;
-        GM_setValue('checkbox', ckBox.checked);
+        GM_setValue('refills_checkbox', ckBox.checked);
     }
 
     function onRefillClick(e) {
-        var $el = $(this);
         let target = e.target;
         var parent = e.target.parentElement;
         if (!safetyOn) return;
@@ -50,20 +53,21 @@
         if (!titleBar) return setTimeout(function (){userQueryCB(responseText, id, param)}, 100);
         $(titleBar).append(safetyNet);
 
-        let ckBox = document.querySelector("#confirm");
+        let ckBox = document.querySelector("#refill_confirm");
         ckBox.addEventListener("click", onCheckboxClicked);
-        ckBox.checked = safetyOn = GM_getValue('checkbox', true);
+        ckBox.checked = safetyOn = GM_getValue('refills_checkbox', true);
 
         document.querySelector("#mainContainer > div.content-wrapper > ul").addEventListener('click', onRefillClick, {capture: true}, true);
     }
- 
+
     //////////////////////////////////////////////////////////////////////
-    // Main. 
+    // Main.
     //////////////////////////////////////////////////////////////////////
 
     logScriptStart();
     validateApiKey();
     versionCheck();
+
     xedx_TornUserQuery(null, 'bars', userQueryCB);
 
 })();
