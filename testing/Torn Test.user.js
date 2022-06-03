5cf8993b95e520d08dd69c0a6f03e3ceff2f5988
edlau2 <edlau@ntplx.net>
2022-06-03 04:31:38 -0400

    Update Torn Test.user.js

--- testing/Torn Test.user.js
+++ testing/Torn Test.user.js
@@ -9,6 +9,7 @@
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @grant        GM_addStyle
 // @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        unsafeWindow
 // ==/UserScript==
 
@@ -17,41 +18,69 @@
 /*eslint no-multi-spaces: 0*/
 
 (function() {
-    'use strict'
+    'use strict';
 
     logScriptStart();
 
-    GM_addStyle(".xshow {display: block;} .xhide {display: none;}");
+    GM_addStyle(`.xedx-container {position:fixed; width:650px; overflow:hidden; left:32%; z-index:28; height:326px; top:118px;}
+                 .xedx-iframe {width: 100%;height: 100%;}
+                 .xhide {display: none;}
+                 .xshow {display: block;}
+                 `);
 
-  $('.tt-points-value').mousedown (function() {
-       log('Setting .myPopups to "show" in one sec');
-       setTimeout (function () {
-           log("myPopups: ", $('.myPopups'));
-           $('.myPopups').addClass('xshow').removeClass('xhide');
-       }, 1000);});
+    $('.toggle-content___C6m1T span:contains("Points")').next().mousedown(function() {     //  Pick MouseDown event
+        log('Mouse click detected');
+        setTimeout(function() {
+            log('timer popped: ', $('#myIframe'));
+            hideFrameContent();  // Hide the stuff we don't want.
+            $('#myIframe').removeClass('xhide').addClass('xshow');
+        }, 1000);
+    });
 
-    // Get a copy of the e-bar.
-    let eBar = document.querySelector("#refill-energy-bar");
-    let orgEnergyBar = $(eBar).clone();
-    $(orgEnergyBar).addClass('myPopups xhide'); // Set class so I can find it, and to hide it.
-    log('orgEnergyBar: ', orgEnergyBar);
+    const content = "<div id='myIframe' class='myframe xedx-container xhide'" +
+          "><iframe id='x-iframe' class='xedx-iframe' src='https://www.torn.com/points.php'></iframe></div>";
 
-    // Saved for now
-    let myFrameContent = "<div style='position: fixed; width:1000px; height:500px; top:200px; left:775px; z-index:999;'" +
-       "class='myPopups xhide'> <iframe id='pointspopup' style='width: 1000px; height: 500px;'" +
-       "src='https://www.torn.com/points.php'> </iframe> </div>"
+    // This is so you don't load iFrames inside of iFrames forever, if you click on the points in the iFrame :-)
+    if (window.top === window.self) {     // Run Script if the Focus is Main Window
+        let iframe = $("body").prepend(content);
+    } else {
+        //--- Script is on domain_B.com if it is IN AN IFRAME.
+        // DO YOUR STUFF HERE.
+    }
 
-    let iframe = $("body").prepend(orgEnergyBar);
+    //$('iframe').load(function () { // This runs when the iFrame loads? I just run it when I unhide it.
+    function hideFrameContent() {
+        //var frame = $('iframe').contents();
+        var frame = $('#x-iframe').contents(); // I like finding ID's better if unique.
+                                               // Only need classes to match multiples.
+        log('[test] frame: ', frame);
 
-    // Not dong this ATM...
-    //
-    // Note sure why this is here, but hides everything, so need to unhide everything if you want to see it.
-    // If this is what I think it is, this is what I meant about needing to be sure your parent divs aren't hidden.
-    if (window.top === window.self) {
-        let res = $("#pointspopup").contents().find("body");
-        log('res: ', res);
-        //$("#pointspopup").contents().find("body").hide(); // Comment this out, and you'll see stuff again
-  }
+        // This adds frame-specific styles.
+        frame.find('head').append(`<style type="text/css">
+            .points-list > li {display:none!important;}
+            .points-list > li:nth-child(2) {display:block!important;}
+            .points-list > li:nth-child(3) {display:block!important;}
+            .points-list > li:nth-child(4) {display:block!important;}
+            // .content-wrapper{
+            // width:100%!important;
+            // }
+            </style>`);
 
+        // Slectively hide content, so the parents stay visible.
+        frame.find('.tutorial-cont,#sidebarroot,#header-root,#footer,.chat-box-wrap_3fvIp,.content-title').hide();
+        frame.find('div[class^="_chat-box"]').hide();
+
+        // Get rid of top margin
+        frame.find('.content-wrapper').css({"margin-top": "0px"});
+
+        // Make sure the first 3 icons (0, 1, and 2) are shown, the rest - just remove.
+        frame.find('.points-list > li').each(function (index) {
+            if (index <= 2) {
+                $(this).show(); // Hidden already by css styles, above (I think): ".points-list > li {display:none!important;}"
+            } else {
+                $(this).remove();
+            }
+        });
+    }
 
 })();
