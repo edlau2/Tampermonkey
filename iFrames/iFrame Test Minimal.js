bc5a1b877b92e34e350990c12237e19a39df80c9
edlau2 <edlau@ntplx.net>
2022-06-16 22:30:50 -0400

    Update iFrame Test Minimal.js

--- iFrames/iFrame Test Minimal.js
+++ iFrames/iFrame Test Minimal.js
@@ -5,14 +5,16 @@ $('#user-money').mouseenter(function(){setTimeout(function() {$('#ivault').show(
 // Click OutSide to Hide iFrame
 $('body').click(function() {$('#ivault').hide()});
 
-function hideHeader() {
-    if ($('#ivault').length) {
-        if ($("#ivault").contents().find("#header-root").length) {
-            $("#ivault").contents().find("#header-root").hide();
-        } else {
-            setTimeout(hideHeader, 250);
-        }
+function checkIframeLoaded(firstCheck=false) {
+    var iframe = document.getElementById('ivault');
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    if (iframeDoc && iframeDoc.readyState == 'complete') {
+        if (firstCheck) return window.setTimeout(checkIframeLoaded, 250); // Ignore first #document complete.
+        $("#ivault").contents().find("#header-root").hide(); // Hide stuff, add more with commas
+        return;
     }
+    window.setTimeout(checkIframeLoaded, 250); // If we are here, it is not loaded.
 }
 
 if (window.top === window.self) {
@@ -20,5 +22,5 @@ if (window.top === window.self) {
                 "style='display:none; position:fixed; width:850px; height:326px; left:34%; top:13%;" +
                 "z-index:99; border:10px solid #1a0029 ; outline:1px solid #f50'" +
                 "src= 'https://www.torn.com/properties.php#/p=options&tab=vault' </iframe>");
-    hideHeader();
+    checkIframeLoaded(true);
 }
