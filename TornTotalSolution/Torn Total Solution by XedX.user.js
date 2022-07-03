9d3b419f0d6e71f2ca9c36b01debaeb53bb4eab4
edlau2 <edlau@ntplx.net>
2022-07-03 15:16:14 -0400

    Add Torn Criminal Record Details script

--- TornTotalSolution/Torn Total Solution by XedX.user.js
+++ TornTotalSolution/Torn Total Solution by XedX.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Total Solution by XedX
 // @namespace    http://tampermonkey.net/
-// @version      2.7
+// @version      2.8
 // @description  A compendium of all my individual scripts for the Home page
 // @author       xedx [2100735]
 // @match        https://www.torn.com/*
@@ -6599,7 +6599,7 @@
     // TBD!
     function tornGymGains() {
 
-        return _tornAmmoMods();
+        return _tornGymGains();
 
         function _tornGymGains() {
             log('[tornGymGains]');
@@ -6617,6 +6617,123 @@
     function removeTornGymGains() {
 
     }
+
+    //////////////////////////////////////////////////////////////////////////////////
+    // Handlers for "Torn Criminal Record Details" (called on page complete)
+    //////////////////////////////////////////////////////////////////////////////////
+
+    /*
+    Borrowed from:
+        @name                Alteracoes
+        @author              magno
+    */
+
+    function tornCrimeDetails() {
+
+        return _tornCrimeDetails();
+
+        function _tornCrimeDetails() {
+            log('[tornCrimeDetails]');
+
+            return new Promise((resolve, reject) => {
+                if (abroad()) return reject('[tornCrimeDetails] not at home!');
+
+                let arrayThefts = [[1000, 'Sneak&nbsp;Thief'], [2500, 'Prowler'], [5000, 'Safe&nbsp;Cracker'], [7500, 'Marauder'], [10000, 'Cat&nbsp;Burgler'], [12500, 'Pilferer'], [15000, 'Desperado'], [17500, 'Rustler'], [20000, 'Pick-Pocket'], [22500, 'Vandal'], [25000, 'Kleptomaniac']];
+                let arrayVirus = [[500, 'Ub3rn00b&nbsp;Hacker'], [1000, 'N00b&nbsp;Hacker'], [1500, '1337n00b&nbsp;Hacker'],
+                                  [2000, 'Ph34r3dn00b&nbsp;Hacker'], [2500, 'Ph34r3d&nbsp;Hacker'], [3000, 'Ph343d1337&nbsp;Hacker'],
+                                  [3500, 'Ub3rph34r3d&nbsp;Hacker'], [4000, 'Ub3r&nbsp;Hacker'], [4500, '1337&nbsp;Hacker'],
+                                  [5000, 'Ub3r1337&nbsp;Hacker'], [5500, 'Key&nbsp;Puncher'], [6000, 'Script&nbsp;Kid'], [7000, 'Geek Speak'], [8000, 'Techie'], [9000, 'Cyber Punk'], [10000, 'Programmer']];
+                let arrayMurder = [[1000, 'Beginner&nbsp;Assassin'], [2000, 'Novice&nbsp;Assassin'], [3000, 'Competent&nbsp;Assassin'],
+                                   [4000, 'Elite&nbsp;Assassin'], [5000, 'Deadly&nbsp;Assassin'], [6000, 'Lethal&nbsp;Assassin'], [7000, 'Fatal&nbsp;Assassin'], [8000, 'Trigger&nbsp;Assassin'], [9000, 'Hit&nbsp;Man'], [10000, 'Executioner']];
+                let arrayDrugs = [[250, 'Drug&nbsp;Pusher'], [500, 'Drug&nbsp;Runner'], [1000, 'Drug&nbsp;Dealer'],
+                                  [2000, 'Drug&nbsp;Lord'], [4000, 'Candy Man'], [6000, 'Connection'], [8000, 'King Pin'], [10000, 'Supplier']];
+                let arrayFraud = [[300, 'Fake'], [600, 'Counterfeit'], [900, 'Pretender'], [1200, 'Clandestine'],
+                                  [1500, 'Imposter'], [2000, 'Pseudo'], [2500, 'Imitation'],
+                                  [3000, 'Simulated'], [3500, 'Hoax'], [4000, 'Faux'],
+                                  [5000, 'Poser'], [6000, 'Deception'], [7000, 'Phony'], [8000, 'Parody'], [9000, 'Travesty'], [10000, 'Pyro']];
+                let arrayGTA = [[200, 'Gone&nbsp;In&nbsp;300&nbsp;Seconds'], [400, 'Gone&nbsp;In&nbsp;240&nbsp;Seconds'], [600, 'Gone&nbsp;In&nbsp;180&nbsp;Seconds'],
+                                [800, 'Gone&nbsp;In&nbsp;120&nbsp;Seconds'], [1000, 'Gone&nbsp;In&nbsp;60&nbsp;Seconds'], [1200, 'Gone&nbsp;In&nbsp;30&nbsp;Seconds'],
+                                [1500, 'Gone&nbsp;In&nbsp;45&nbsp;Seconds'], [2000, 'Gone&nbsp;In&nbsp;15&nbsp;Seconds'], [2500, 'Booster'],
+                                [3000, 'Joy&nbsp;Rider'], [3500, 'Super&nbsp;Booster'], [4000, 'Master&nbsp;Carjacker'],
+                                [4500, 'Slim&nbsp;Jim'], [5000, 'Novice&nbsp;Joy&nbsp;Rider'], [5500, 'Novice&nbsp;Slim&nbsp;Jim'],
+                                [6000, 'Professional&nbsp;Joy&nbsp;Rider'], [6500, 'Professional&nbsp;Booster'], [7000, 'Professional&nbsp;Slim&nbsp;Jim'],
+                                [8000, 'Master&nbsp;Joy&nbsp;Rider'], [9000, 'Master Booster'], [10000, 'Master Slim Jim']];
+
+                let arrayIllegal = [[5000,'Civil&nbsp;Offence']];
+                let arrayOther = [[5000,'Find&nbsp;A&nbsp;Penny,&nbsp;Pick&nbsp;It&nbsp;Up']];
+
+                GM_addStyle(`
+                    .fr60 {float: right; width:60px;}
+                    .cdata {font-style:italic; float:right; width:75px; text-align:left}
+                    .block {display:block;overflow:hidden;}
+                    .boldred {font-weight:bold; color:red; width:35px; float:right; text-align:left;}
+                `);
+
+            $("div[id^=item]:has(>div.title-black:contains('Criminal Record'))" ).find('li').each(
+                function(item){
+                    let arr = null;
+                    let type = $(this).children(":first").text().trim();
+                    let desc = $(this).children(":last").text();
+                    let n = desc.replace(',','');
+
+                    switch(type){
+                        case 'Other':
+                            type += ' (nerve: 2)';
+                            arr = arrayOther;
+                            break;
+                        case 'Illegal products':
+                            type = 'Illegal products (nerve: 3, 16)';
+                            arr = arrayIllegal;
+                            break;
+                        case 'Theft':
+                            type += ' (nerve: 4, 5, 6, 7, 15)';
+                            arr = arrayThefts;
+                            break;
+                        case 'Computer crimes':
+                            type += ' (nerve: 9, 18)';
+                            arr = arrayVirus;
+                            break;
+                        case 'Murder':
+                            type += ' (nerve: 10)';
+                            arr = arrayMurder;
+                            break;
+                        case 'Drug deals':
+                            type += ' (nerve: 8)';
+                            arr = arrayDrugs;
+                            break;
+                        case 'Fraud crimes':
+                            type = 'Fraud (nerve: 11, 13, 14, 17)';
+                            arr = arrayFraud;
+                            break;
+                        case 'Auto theft':
+                            type += ' (nerve: 12)';
+                            arr = arrayGTA;
+                            break;
+                    }
+                    $(this).children(":first").text(type);
+
+                    if (arr != null) {
+                        var mink = -1;
+                        for (var k=0; k<arr.length; ++k) {
+                            if ((mink == -1) && (arr[k][0] > n)) mink = k;
+                        }
+                        if (mink >= 0) {
+                            desc = '<span class="fr60">'+desc+'</span><span class="cdata block">' + arr[mink][1] +
+                                   '</span><span class="boldred">' + (arr[mink][0] - n) + '</span>';
+                            $(this).children(":last").html(desc);
+                            $(this).children(":last").attr('title', desc);
+                        }else{
+                            $(this).children(":last").css("color","green");
+                            $(this).children(":last").html('<span class="fr60">'+desc+'</span><span class="cdata">Good job!</span>');
+                        }
+                    }
+                });
+
+                resolve("[tornCrimeDetails] startup complete!");
+            })
+        }
+
+    } // End function tornCrimeDetails() {
 
     ///////////////////////////////////////////////////////////////////////////////////
     //
@@ -6854,6 +6971,8 @@
                          tornCustomizableSidebar, removeCustomizableSidebar, generalToolTip, 'all');
         setGeneralCfgOpt("tornTTFilter", "Torn TT Filter",
                          tornTTFilter, null, ttFilterTt, "all", true);
+        setGeneralCfgOpt("tornCrimeDetails", "Torn Criminal Record Details",
+                         tornCrimeDetails, null, generalToolTip, "home");
 
         // BAZAAR (and shops)
         setGeneralCfgOpt("tornBazaarPlus", "Torn Bazaar Pricing, Plus!",
@@ -7449,11 +7568,14 @@
         // Currently, underneath the indicator of which server we're connected to.
         installConfigMenu();
 
-        if (opts_enabledScripts.crimeToolTips.enabled) {tornCrimeTooltips().then(a => _a(a), b => _b(b));}
-
         if (opts_enabledScripts.tornTTFilter.enabled) {tornTTFilter().then(a => _a(a), b => _b(b));}
 
         if (opts_enabledScripts.tornFacPageSearch.enabled) {tornFacPageSearch().then(a => _a(a), b => _b(b));}
+
+        if (isIndexPage()) {
+            if (opts_enabledScripts.crimeToolTips.enabled) {tornCrimeTooltips().then(a => _a(a), b => _b(b));}
+            if (opts_enabledScripts.tornCrimeDetails.enabled) {tornCrimeDetails().then(a => _a(a), b => _b(b));}
+        }
 
         if (isUserListPage()) {
             if (opts_enabledScripts.tornUserList.enabled) {tornUserList().then(a => _a(a), b => _b(b));}
