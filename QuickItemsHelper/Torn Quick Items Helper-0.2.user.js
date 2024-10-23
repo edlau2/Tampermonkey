4d49d6fc51e12246d7911c536e550a408a8e9382
edlau2 <edlau@ntplx.net>
2024-10-23 17:58:09 -0400

    Add files via upload

--- QuickItemsHelper/Torn Quick Items Helper-0.2.user.js
+++ QuickItemsHelper/Torn Quick Items Helper-0.2.user.js
@@ -7,7 +7,7 @@
 // @match        https://www.torn.com/item.php*
 // @connect      api.torn.com
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
-// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers-2.43.js
+// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers-2.45.7.js
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
@@ -30,7 +30,7 @@
     var catComplete = 0;
 
     function getItemCategory(category) {
-        log("getItemCategory: ", category);
+        debug("getItemCategory: ", category);
 
         if (!$("#xedx-fake").length) {
             let fakeDiv = "<div id='xedx-fake' style='display: none;'><ul id='fake-ul'></ul></div>";
@@ -45,6 +45,14 @@
                         prevtotal: 0};
 
         $.post( URL, postData, processLoadResponse);
+    }
+
+    function hashChangeHandler(e) {
+        log("hashChangeHandler e: ", e);
+    }
+
+    function pushStateChanged(e) {
+        log("hashChangeHandler e: ", e);
     }
 
     function processLoadResponse(response, status, xhr) {
@@ -72,16 +80,72 @@
         }
     }
 
+    function updateQty(e) {
+        let target = e.currentTarget;
+        let textNode = $(target).find(".text")[0];
+
+        let qty = $(textNode).attr("data-qty");
+        let txt = $(textNode).text();
+
+        let parts = txt.split('(');
+        let newQty = parseInt(qty) - 1;
+        let newTxt = parts[0] + " (x" + newQty + ")";
+
+        $(textNode).attr("data-qty", newQty);
+        $(textNode).text(newTxt);
+
+    }
+
+    // Just keep list of ID's for cat:
+    // UL, id="energy-d-items", medical-items, drugs-items, alcohol-items
+    // find LI, data-item == itemId
+    // Find "button" class = option-use wai-btn
+    function addHandlerForUse(itemId, category) {
+        log("[addHandlerForUse] item: ", itemId, " cat: ", category);
+        let node2;
+
+        // Can use switch in JS on text!
+        if (category == "Energy Drink")
+            node2 = $("#energy-d-items");
+        if (category == "Medical")
+            node2 = $("#medical-items");
+        if (category == "Alcohol")
+            node2 = $("#alcohol-items");
+        if (category == "Drug")
+            node2 = $("#drugs-items");
+
+        let elem2 = $($(node2)[0]).find("[data-item='" + itemId + "']")[0];
+        if ($(elem2).length > 0) log("[addHandlerForUse] found elem2");
+        let elem3 = $(elem2).find("[data-action='use']");
+        if ($(elem3).length > 0) log("[addHandlerForUse] found elem3: ", $(elem3));
+
+        //$(elem3).attr("style", "box-shadow: inset 1px 0px 0px 2px #0eb30e;");
+    }
+
+    function updateHandlersForUse() {
+        // called on hash change/push state to add the above?
+    }
+
     function parseItemList(items) {
         debug("Item list: ", $(quickItemsList));
 
         for (let idx=0; idx < $(quickItemsList).length; idx++) {
             let elem = $(quickItemsList)[idx];
+            $(elem).on('click', updateQty);
             let itemId = $(elem).attr("data-id");
             let textNode = $(elem).find(".text")[0];
             let text = $(textNode).text();
             let itemInList = allItemList[itemId];
-            if (itemInList) $(textNode).text(text + " (x" + itemInList.qty + ")");
+            if (itemInList) {
+                $(elem).attr("data-category", itemInList.category);
+                $(textNode).text(text + " (x" + itemInList.qty + ")");
+                $(textNode).attr("data-qty", itemInList.qty);
+                //$(textNode).on('click', updateQty);
+
+                // If we are on the correct category page, add a handler
+                // to the items 'use' btn to updatethen, as well.
+                addHandlerForUse(itemId, itemInList.category);
+            }
         }
     }
 
@@ -116,7 +180,10 @@
     logScriptStart();
     //validateApiKey();
     versionCheck();
+    addBorderStyles();
 
+    installPushStateHandler(pushStateChanged);
+    callOnHashChange(hashChangeHandler);
     callOnContentLoaded(handlePageLoad);
 
 })();
