0e41a4c9b7b61c412b9630404302a3d66a978c01
edlau2 <edlau@ntplx.net>
2024-12-31 15:56:27 -0500

    Add timer until your OC is due

--- OCSort/Torn OC Sort.user.js
+++ OCSort/Torn OC Sort.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Torn OC Sort
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Sort crimes, show missing members, etc
 // @author       xedx [2100735]
-// @match        https://www.torn.com/factions.php*
+// @match        https://www.torn.com/*
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
 // @connect      api.torn.com
 // @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
@@ -26,25 +26,50 @@
 (function() {
     'use strict';
 
+    // Constants used, some to load options..hence first.
     const userId = getThisUserId();    // Used to highlight yourself in list
-
-    // Extra debug logging, for development
-    const logFullApiResponses = GM_getValue("logFullApiResponses", false);
-    debugLoggingEnabled = GM_getValue("debugLoggingEnabled", false);
-
     const keepLastStateKey = "keepLastState";
     const lastStateKey = "lastState";
     const stateOpen = "visible";
     const stateClosed = "hidden";
     const sortKey = 'data-sort';
 
+    // ======================== Configurable Options =======================
+    //
+    // Extra debug logging, for development
+    const logFullApiResponses = GM_getValue("logFullApiResponses", false);
+    debugLoggingEnabled = GM_getValue("debugLoggingEnabled", false);
+
+    // Can edit this, but there's a hidden checkbox for it.
+    var keepLastState = GM_getValue(keepLastStateKey, false);
+
+    // Monitor and display when an OC you are in is due
+    var trackMyOc = GM_getValue("trackMyOc", true);
+
+    // ====================== End Configurable Options ======================
+
+    var lastState = GM_getValue(lastStateKey, stateClosed);
+
+    // Can run the OC watcher, to track when yours is due,
+    // independently. Just start it and return if not on fac page.
+    //
+    var myCrimeId = GM_getValue("myCrimeId", null);
+    var myCrimeStartTime = GM_getValue("myCrimeStartTime", 0);
+
+    const secsInDay = 24 * 60 * 60;
+    const secsInHr = 60 * 60;
+
+    if (trackMyOc == true) {
+        logScriptStart();
+        validateApiKey()
+        startMyOcTracking();
+        if (!isFactionPage()) return;
+    }
+
     // Sorting criteria
     const sortByTime = 1;
     const sortByLevel = 2;
 
-    // Can edit this, but there's a hidden checkbox for it.
-    var keepLastState = GM_getValue(keepLastStateKey, false);
-    var lastState = GM_getValue(lastStateKey, stateClosed);
 
     // Will reset at page refresh, not permanent cache
     const cacheCompletedCrimes = true;
@@ -82,6 +107,168 @@
         let page = (idx == 0 ? "Recruiting" : idx == 1 ? "Planning" : idx == 2 ? "Completed": "Unknown");
         debug("On index ", idx, " page is '", page, "'", " sortable? ", isSortablePage());
     }
+
+    // ======================= My OC Tracking, see when due ========================
+
+    // Save cached crime data
+    function saveMyCrimeData() {
+        GM_setValue("myCrimeId", myCrimeId);
+        GM_setValue("myCrimeStartTime", myCrimeStartTime);
+    }
+
+    function setTrackerTime(time) {
+        $("#oc-tracker-time").text(time);
+    }
+
+    function nn(t) {return t == 0? '00' : t < 10 ? '0' + t : t;}
+    function getTimeUntilOc() {
+        let now = new Date();
+        let dt = new Date(myCrimeStartTime * 1000);
+
+        let diffSecs = +myCrimeStartTime - now.getTime()/1000;
+        let days = Math.floor(diffSecs / secsInDay);
+        let remains = (diffSecs - (days * secsInDay));
+        let hrs = Math.floor(remains / secsInHr);
+        remains = remains - (hrs * secsInHr);
+        let mins = Math.floor(remains/60);
+
+        let timeStr = "OC in " + days + "d " + nn(hrs) + "h " + nn(mins) + "m";
+        return timeStr;
+    }
+
+    // See if the cached crime has expired/completed.
+    // Return false if invalid/expired/not set, true otherwise.
+    function validateSavedCrime(crimes) {
+        debug("validateSavedCrime: ", myCrimeStartTime, myCrimeId);
+
+        if (!myCrimeStartTime || !myCrimeId) return false;
+
+        let now = new Date();
+        let crimeTime = new Date(myCrimeStartTime * 1000);
+
+        log("now: ", now.toString());
+        log("crimeTime: ", crimeTime.toString());
+
+        // If now is after the start time, expired...
+        if (now.getTime() > myCrimeStartTime * 1000) {
+            myCrimeId = null;
+            myCrimeStartTime = 0;
+            saveMyCrimeData();
+            log("Expired crimeTime");
+            return false;
+        }
+
+        // Cached time can be used...
+        // Should prob validate is still in list,
+        // can be cancelled?
+        return true;
+    }
+
+    function myOcTrackingCb(responseText, ID, param) {
+        debug("myOcTrackingCb");
+        let jsonObj = JSON.parse(responseText);
+        let crimes = jsonObj.crimes;
+        let myCrime, readyAt;
+
+        // Check to see if we can use cached crime time....
+        if (validateSavedCrime(crimes) == true) {
+            debug("Found valid cached start time!");
+        } else {
+            debug("Searching for my crime...");
+            for (let crimeIdx=0; crimeIdx < crimes.length; crimeIdx++) {
+                let crime = crimes[crimeIdx];
+                let slots = crime.slots;
+                for (let slotIdx=0; slotIdx<slots.length; slotIdx++) {
+                    let slot = slots[slotIdx];
+                    if (slot.user_id == userId) {
+                        debug("Found match: ", crime);
+                        myCrime = crime;
+                        readyAt = crime.ready_at;
+                        break;
+                    }
+                }
+                if (myCrime) break;
+            }
+
+            if (myCrime && readyAt) {
+                myCrimeId = myCrime.id;
+                myCrimeStartTime = readyAt;  // Prob need conversion....
+                saveMyCrimeData();
+            } else {
+                debug("Didn't locate my own OC!");
+            }
+        }
+
+        if (myCrimeStartTime) {
+            let dt = new Date(myCrimeStartTime * 1000);
+            debug("Found my OC start time: ", dt.toString());
+
+            setTrackerTime(getTimeUntilOc());
+            setInterval(updateTrackerTime, 31000);
+        }
+
+        function updateTrackerTime() {
+            setTrackerTime(getTimeUntilOc());
+        }
+    }
+
+
+    function startMyOcTracking() {
+        // Add necessary UI components
+
+        debug("Adding UI components");
+        let elem = getMyOcTrackerDiv();
+        makeXedxSidebarContentDiv();
+
+        debug("Made sidebar content: ", $("#x-scrollbar-content"));
+
+        $("#x-scrollbar-content").append(elem);
+        $("#x-scrollbar-content").css("padding", "0px");
+        debug("Added tracker div: ", $("#ocTracker"));
+
+        // On fac page - we will be doing this call anyways,
+        // and the callback will call the aboce callback as well.
+        // So just return.
+        if (isFactionPage()) {
+            debug("startMyOcTracking: on fac page, will check later...");
+            return;
+        }
+
+        debug("startMyOcTracking: must not be fac page?");
+        var options = {"cat": 'planning', "offset": "0", "param": 'ocTracking'};
+        xedx_TornFactionQueryv2("", "crimes", myOcTrackingCb, options);
+    }
+
+    function getMyOcTrackerDiv() {
+
+        GM_addStyle(`
+            #ocTracker {
+                display: flex;
+            }
+
+            #ocTracker span {
+                align-content: center;
+                justify-content: center;
+                display: flex;
+                flex-flow: row wrap;
+                padding: 3px;
+
+                font-size: 14px;
+                width: 90%;
+                cursor: pointer;
+            }
+
+        `);
+        let myDiv = `
+            <div id="ocTracker">
+               <span id="oc-tracker-time" class="xtime">00:00:00</span>
+           </div>
+        `;
+
+        return myDiv;
+    }
+
+    // ======================== Sorting portion of script ===========================
 
     function handlePageChange() {
         logCurrPage();
@@ -518,6 +705,10 @@
             return completedCrimesCb(crimes);
         }
 
+        if (trackMyOc == true && param == "planning") {
+            myOcTrackingCb(responseText, ID, param);
+        }
+
         crimes.forEach(function (crime, index) {
             crime.slots.forEach(function (slot, index) {
                 if (slot.user_id) plannedCrimeMembers.push(slot.user_id);
@@ -573,10 +764,12 @@
     // Main.
     //////////////////////////////////////////////////////////////////////
 
-    logScriptStart();
+    if (trackMyOc != true) logScriptStart();
+
     if (checkCloudFlare()) return log("Won't run while challenge active!");
 
-    validateApiKey();
+    if (trackMyOc != true) validateApiKey();
+
     versionCheck();
     addStyles();
 
