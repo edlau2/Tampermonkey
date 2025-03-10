06a77956b20f509b68bbd75d788fd9918d1fa0dd
edlau2 <edlau@ntplx.net>
2025-03-09 21:39:52 -0400

    Add files via upload

--- StickyChainReports/Torn Sticky Chain Report.user.js
+++ StickyChainReports/Torn Sticky Chain Report.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Torn Sticky Chain Report
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.7
 // @description  Make header sticky on war/chain report
 // @author       xedx [2100735]
 // @match        https://www.torn.com/war.php*
 // @match        https://www.torn.com/factions.php*
 // @icon         https://www.google.com/s2/favicons?domain=torn.com
 // @connect      api.torn.com
-// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers-2.45.7.js
+// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @require      http://code.jquery.com/ui/1.12.1/jquery-ui.js
 // @grant        GM_addStyle
@@ -29,56 +29,46 @@
 (function() {
     'use strict';
 
-    var userId = GM_getValue("userId", undefined);
+    debugLoggingEnabled = GM_getValue("debugLoggingEnabled", false);
+    const userId = getPlayerId();
+    var warPage = false;
     var myLi;
+    var myFacTarget;
 
-    // Move to shared lib...
-    function getId() {
-        if (userId) {
-            return;
-        }
-        let tmp = $("#torn-user").val();
-        if (!tmp) return;
-        let parts = tmp.split('"');
-        if (!parts || parts.length < 4) return;
-        userId = parts[3];
-        GM_setValue("userId", userId);
-
-    }
-
-    function handleSortClick(e) {
-        if (findMyLi()) {
-            scrollTo(myLi);
-        }
-    }
+    function handleSortClick(e) {if (findMyLi(myFacTarget)) {scrollTo(myLi);}}
 
     function scrollTo(elem) {
-        let target = $(".report-members-stats-content")[0];
+        if (!myFacTarget) {
+            console.error("No target set!");
+            debugger;
+            return;
+        }
+        debug("scrollTo: ", $(elem));
         let scrollTo = $(elem);
-        let pos = $(elem).offset().top - $(target).offset().top + $(target).scrollTop() - 150;
-        displayHtmlToolTip($(elem), ("#" + $(elem).index()), "tooltip4");
-
-       $(target).animate({
-          scrollTop: pos
-       });
+        let pos = $(elem).offset().top - $(myFacTarget).offset().top + $(myFacTarget).scrollTop() - 150;
+        displayHtmlToolTip($(elem), ("#" + ($(elem).index()+1)), "tooltip4");
+        $(myFacTarget).animate({scrollTop: pos});
     }
 
     var classAdded = false;
-    function findMyLi() {
-        let honors = $("[class^='honorWrap_'] > a");
+    function findMyLi(root) {
+        let honors = $(root).find("[class^='honorWrap_'] > a");
         for (let idx=0; idx < $(honors).length; idx++) {
             let node = $(honors)[idx];
             let href = $(node).attr("href");
             if (href.indexOf(userId) > -1) {
                 myLi = $(node).parent().parent().parent();
-
+                if (warPage == true) myLi = $(myLi).closest("li");
                 let infoBox = $(myLi).find("[class*='userInfoBox_']");
-                log("info-box: ", $(infoBox));
-                $(infoBox).addClass("xedx-stb");
+                if (warPage == false) $(infoBox).addClass("xedx-stb");
                 let memRows = $(".members-stats-rows > li.members-stats-row");
                 let idx = $(myLi).index();
                 let member = $(memRows)[idx];
-                $(member).addClass("xedx-stb");
+                if (warPage == true)
+                    $(myLi).addClass("xedx-stb");
+                else
+                    $(member).addClass("xedx-stb");
+                debug("Found my LI: ", $(myLi));
                 return myLi;
             }
         }
@@ -105,11 +95,12 @@
 
     // War report - add area to enter chain ID
     function doWarPageLoad(retries=0) {
+        debug("doWarPageLoad");
         if ($("#xchain-span").length > 0) return;
         let title = $("#skip-to-content");
         if (!$(title).length) {
             if (retries++ < 20) return setTimeout(doWarPageLoad, 500, retries);
-            return log("Too many retries!");
+            return log("[doWarPageLoad] Too many retries!");
         }
         let node = getSpanForChainId();
         $(title).after(node);
@@ -120,11 +111,9 @@
         let link = $(".report-link");
         if (!$(link).length) {
             if (retries++ < 30) return setTimeout(doFacPageLoad, 200, retries);
-            log("too many retries");
+            debug("[doFacPageLoad] too many retries");
             return;
         }
-
-        log("Got link: ", $(link));
 
         let url = $(link).attr("href");
         let uberDiv = "<div id='xwrap'></div>";
@@ -137,45 +126,81 @@
 
     // Main entry once page loads
     function handlePageLoad(retries=0) {
+        debug("handlePageLoad");
+
+        let params = new URLSearchParams(location.search);
+
         let href = location.href;
-        if (href.indexOf("factions.php") > -1) {
-            doFacPageLoad();
-            return;
-        }
+        if (href.indexOf("factions.php") > -1) { return doFacPageLoad(); }
 
         // Ranked War report - can lookup chain IDs?
         // Iterate backwards, use "to="
         // https://api.torn.com/faction/?selections=chains&to=1729850258&key=
-        if (href.indexOf("war.php?step=rankreport" > -1)) {
+        if (params.get("step") == "rankreport") {
+            warPage = true;
             doWarPageLoad();
         }
 
-        let target = $(".report-members-stats-content")[0];
-        if(!$(target).length) {
-            if (retries++ < 10) return setTimeout(handlePageLoad, 250);
-            return log("Too many attempts...");
+        let enemyFacTarget;
+        let selector = ".report-members-stats-content";
+        if (warPage == true) {
+            selector = ".your-faction .members-cont";
+            myFacTarget = $(selector)[0];
+            enemyFacTarget = $(".enemy-faction .members-cont")[0];
         } else {
-            $(target).css("max-height", "90vh"); // bottom margin for TTS footer.
-            $(target).css("overflow-y", "auto");
-            $(target).css("top", "0px");
-            $(target).css("position", "sticky");
-            let hdrs = $("ul.report-stats-titles").css({"position": "sticky", "top": 0});
+            myFacTarget = $(selector)[0];
         }
 
-        if (findMyLi()) scrollTo(myLi);
+        if(!$(myFacTarget).length) {
+            if (retries++ < 40) return setTimeout(handlePageLoad, 250, retries);
+            return log("[handlePageLoad] Too many attempts for '", selector, "'");
+        }
+
+        $(myFacTarget).addClass("sticky-wrap");
+        $(enemyFacTarget).addClass("sticky-wrap");
+        if (warPage == true)
+                $(".members-cont .c-pointer").css({"position": "sticky", "top": 0, "z-index": 9999999});
+            else
+                $("ul.report-stats-titles").css({"position": "sticky", "top": 0, "z-index": 9999999});
+        /*
+        else {
+            $(myFacTarget).addClass("sticky-wrap");
+
+            if (warPage == true)
+                $(".members-cont .c-pointer").css({"position": "sticky", "top": 0, "z-index": 9999999});
+            else
+                $("ul.report-stats-titles").css({"position": "sticky", "top": 0, "z-index": 9999999});
+
+            if ($(enemyFacTarget).length) {
+                $(enemyFacTarget).addClass("sticky-wrap");
+            }
+        }
+        */
+
+        if (findMyLi(myFacTarget)) scrollTo(myLi);
         $(".c-pointer").on('click', function(e) {
             setTimeout(handleSortClick, 250);
         });
     }
 
     logScriptStart();
-
     addToolTipStyle();
     addBorderStyles();
 
-    GM_addStyle(".xedx-stb { background-color: rgba(108,195,21,.07); cursor: pointer;}");
+    GM_addStyle(`
+        .xedx-stb {
+            background-color: rgba(108,195,21,.07) !important;
+            cursor: pointer;
+        }
 
-    callOnContentLoaded(getId);
+        .sticky-wrap {
+            max-height: 90vh;
+            overflow-y: auto;
+            top: 0px;
+            position: sticky;
+        }
+    `);
+
     callOnContentComplete(handlePageLoad);
 
 })();
