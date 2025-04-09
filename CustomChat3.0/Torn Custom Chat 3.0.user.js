bfaa9fd932d775f905d2b6a4a8d95c3a09c331f2
edlau2 <edlau@ntplx.net>
2025-04-09 19:46:57 -0400

    Re-organization, partial class matches

--- CustomChat3.0/Torn Custom Chat 3.0.user.js
+++ CustomChat3.0/Torn Custom Chat 3.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Custom Chat 3.0
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.5
 // @description  This script does...
 // @author       xedx [2100735]
 // @run-at       document-start
@@ -15,6 +15,48 @@
 (function() {
     'use strict';
 
+    // ========== Editable options, will be in a UI menu later ===============
+
+    // Options for fac chat messages
+    const FAC_OPTS = {
+        MY_TEXT_COLOR: '#dd0000',
+        THEM_TEXT_COLOR: 'white',
+        MY_BORDER: '1px solid limegreen',
+        THEM_BORDER: 'none',
+        MY_BG_COLOR: 'black',
+        THEM_BG_COLOR: 'transparent'
+    }
+
+    // Options for private chat messages
+    const PRIV_OPTS = {
+        MY_TEXT_COLOR: 'red',
+        THEM_TEXT_COLOR: 'white',
+        MY_BORDER: '1px solid #000099',
+        THEM_BORDER: 'none',
+        MY_BG_COLOR: 'black',
+        THEM_BG_COLOR: 'transparent'
+    }
+
+    // Applies to all chat msg text
+    const GEN_OPTS = {
+        FONT_FAMILY: 'arial',
+        FONT_SIZE: '14px'
+    }
+
+    // =========== End editable options ===============
+
+    const addDevStyles = false;    // This ATM puts borders on various chat boxes, I use during development to locate elements
+
+    const log = function(...data) {console.log(GM_info.script.name + ': ', ...data);}
+    log("script started");
+
+    const FAC_SEL = `#chatRoot [id^='faction-']`;
+    const PRIV_SEL = `#chatRoot [id^='private-']`;
+
+    const ALL_MSGS_SEL = `[class^='list_'] [class*='root_']:not([class*='divider'])`;
+    const MY_MSGS_SEL = `[class^='list_'] [class*='self_']`;
+    const THEM_MSGS_SEL = `[class^='list_'] > [class*='root_']:not([class*='self_']):not([class*='divider'])`;
+
     const xedx_addStyle = function(styles) {
         (typeof GM_addStyle != "undefined") ?
             GM_addStyle(styles) :
@@ -26,31 +68,71 @@
             }
     };
 
-    xedx_addStyle(`
-        .textarea___V8HsV {
-            height: 76px !important;
+    function classList(element) {if ($(element).attr("class")) return $(element).attr("class").split(/\s+/);}
+
+    let stylesList = "";
+    function addStyleToList(rootSel, opt, val) {
+        let style;
+        let subSel = MY_MSGS_SEL;
+        if (opt.indexOf('THEM') > -1) subSel = THEM_MSGS_SEL;
+
+        if (opt.indexOf('BG_COLOR') > -1) {
+            style = `${rootSel} ${subSel} {
+                background-color: ${val} !important;
+            }`;
         }
-        .item___ydsFW {
-            width: 260px;
+        if (opt.indexOf('TEXT_COLOR') > -1) {
+            style = `${rootSel} ${subSel} span {
+                color: ${val} !important;
+            }`;
         }
-        .root___Xw4jI {
-            font-family: arial !important;
-            font-size: 14px !important;
-            /*color: lightblue !important;*/
-        }
-        .root___NVIc9 {
-            background-color: transparent !important;
-        }
-        .root___NVIc9:not(.self___IbPax) span {
-            color: lightblue !important;
-        }
-        .self___IbPax span {
-            color: red !important;
+        if (opt.indexOf('BORDER') > -1) {
+            style = `${rootSel} ${subSel} {
+                border: ${val} !important;
+            }`;
         }
 
-        .root___NVIc9.self___IbPax {
-            border: 1px solid pink !important;
+        stylesList = stylesList + ' ' + style;
+    }
+
+    let opts = FAC_OPTS;
+    let keys = Object.keys(opts);
+    for (let idx=0; idx<keys.length; idx++) {
+        let opt = keys[idx];
+        let val = opts[opt];
+        if (val == 'none') continue;
+        addStyleToList(FAC_SEL, opt, val);
+    }
+
+    opts = PRIV_OPTS;
+    keys = Object.keys(opts);
+    for (let idx=0; idx<keys.length; idx++) {
+        let opt = keys[idx];
+        let val = opts[opt];
+        if (val == 'none') continue;
+        addStyleToList(PRIV_SEL, opt, val);
+    }
+
+    xedx_addStyle(stylesList);
+
+    xedx_addStyle(`
+        ${FAC_SEL} ${ALL_MSGS_SEL}, ${PRIV_SEL} ${ALL_MSGS_SEL} {
+            font-family: ${GEN_OPTS.FONT_FAMILY} !important;
+            font-size: ${GEN_OPTS.FONT_SIZE} !important;
         }
-    `);
+        `);
+
+    if (addDevStyles == true) {
+        xedx_addStyle(`
+            ${FAC_SEL} {
+                border: 1px solid limegreen;
+                /*width: 260px !important;*/
+            }
+            ${PRIV_SEL} {
+                border: 1px solid blue;
+                /*width: 260px !important;*/
+            }
+        `);
+    }
 
 })();
