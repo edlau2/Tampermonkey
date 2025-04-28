c59f874a0600de8766a63501871a916b9d6a707d
edlau2 <edlau@ntplx.net>
2025-04-28 17:27:37 -0600

    Add files via upload

--- TabTitles/Torn Tab Titles.user.js
+++ TabTitles/Torn Tab Titles.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Tab Titles
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Makes names on attack page tabs easier to see
 // @author       xedx [2100735]
 // @match        https://www.torn.com/loader.php?sid=attack*
@@ -16,15 +16,36 @@
 (function() {
     'use strict';
 
+    const apiKey = 'Your API key here';
+
+    const params = new URLSearchParams(window.location.search);
+    const userID = params.get('user2ID');
     const opts = {attributes: false, childList: true, subtree: true};
     const targetNode = $("title")[0];
-    const fixTab = function (node) {$(targetNode).text($(node).text().replace('Attack', 'A'));}
+    const fixTab = function (node, name) {
+        $(targetNode).text($(node).text()
+                           .replace('Attack', 'A')
+                           .replace(userID, name)
+                           .replace("TORN", name));}
+    const apiURL = `https://api.torn.com/v2/user?selections=basic&id=${userID}&key=${apiKey}`;
+    var name = userID;
 
-    fixTab(targetNode);
+    $.ajax({
+            url: apiURL, type: 'GET',
+            success: function (response, status, xhr) {
+                if (response) name = response.name;
+                fixTab(targetNode, name);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.error("Error in API request/response!");
+            }
+        });
+
+    fixTab(targetNode, name);
 
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
-            fixTab(mutation.target);
+            fixTab(mutation.target, name);
             observer.disconnect();
         });
     });
