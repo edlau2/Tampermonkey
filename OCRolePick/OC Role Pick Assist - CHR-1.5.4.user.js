e07abae295c061c3cc2c7564b3e263a68c007df6
edlau2 <edlau@ntplx.net>
2025-07-04 07:13:49 -0400

    Add files via upload

--- OCRolePick/OC Role Pick Assist - CHR-1.5.4.user.js
+++ OCRolePick/OC Role Pick Assist - CHR-1.5.4.user.js
@@ -29,64 +29,6 @@
 
     debugLoggingEnabled = GM_getValue("debugLoggingEnabled", false);    // Enables more verbose logging
 
-    // ================= experimental, alt way to get crime info ================
-    const hookFetch = false;
-    if (hookFetch == true) {
-        function logResponse(r) {
-            log("[logResponse]:\n", JSON.stringify(r, null, 4));
-        }
-
-        const { fetch: originalFetch } = unsafeWindow;
-        unsafeWindow.fetch = async (...args) => {
-            var [resource, config] = args;
-            var response = await originalFetch(resource, config);
-            const json = () => response.clone().json()
-            .then((data) => {
-                data = { ...data };
-
-                log("URL: ", response.url);
-                if (data?.DB) {
-                    log("[orgFetch] DB entries:");
-                    log(JSON.stringify(data.DB, null, 4));
-                }
-                    //for (let entry of Object.entries(data.DB)) {
-                    //    log(entry);
-                    //}
-                    //log(JSON.stringify(data, null, 4));
-                //}
-
-                /*
-                if(response.url.indexOf('?sid=attackData') != -1) {
-                    if(data.DB.error?.includes('in hospital') || data.DB.error?.includes('unconscious') || data.DB.error?.includes('This fight no longer exists')) {
-                        data.DB.defenderUser.playername += ' [Hospital]'
-                        delete data.DB.error
-                        delete data.startErrorTitle
-                    }
-                }
-
-                if(response.url.indexOf('page.php?sid=factionsProfile&step=getInfo') != -1) {
-                    if (hide_faction_desc === "true") {
-                        data.faction.description = 'Blocked by Walla Walla'
-                    }
-                }
-                */
-
-                return data;
-            })
-
-            response.json = json;
-            response.text = async () =>JSON.stringify(await json());
-
-            // ===========
-            log("response.url: ", response.url);
-            response.json().then( r => logResponse(r));
-            // ===========
-
-
-            return response;
-        };
-    }
-
     // ================= Color Coding the slots, based on CPR ===================
 
     const defaultLevel6 = 70;
