91782e474f6c139947c033a996a10a84c8674388
edlau2 <edlau@ntplx.net>
2025-07-26 18:06:02 -0400

    Update Torn PDA Tests.user.js

--- PdaTests/Torn PDA Tests.user.js
+++ PdaTests/Torn PDA Tests.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn PDA Tests
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.5
 // @description  This script tests for PDA compliance
 // @author       xedx [2100735]
 // @match        https://www.torn.com/*
@@ -24,25 +24,22 @@
     const viewPortWidthPx = window.innerWidth;
     const isMobileView = viewPortWidthPx <= 784;
     const isTampermonkeyEnabled = typeof unsafeWindow !== 'undefined';
+    console.log("isTampermonkeyEnabled: ", isTampermonkeyEnabled, " isMobileView: ", isMobileView);
 
     console.log("Torn PDA Tests started");
-    console.log("Test GM_info.script.name");
+    console.log("GM_info: ", JSON.stringify(GM_info, null, 4));
+    console.log("Script name: ", JSON.stringify(GM_info.script.name, null, 4));
 
-    if (GM_info.script.name === undefined)
+    if (GM_info.script.name === undefined) {
         GM_info.script.name = "Torn PDA Tests";
-
-    try {
         console.log("GM_info: ", JSON.stringify(GM_info, null, 4));
         console.log("Script name: ", JSON.stringify(GM_info.script.name, null, 4));
-        console.log("isTampermonkeyEnabled: ", isTampermonkeyEnabled, " isMobileView: ", isMobileView);
-    } catch (err) {
-        console.log("Failed! ", err);
     }
 
     function log(...data) { console.log(GM_info.script.name + ': ', ...data); };
 
-    console.log("Test 'log' function");
-    log("If you see this it works");
+    log("Test 'addStyle' function");
+    GM_addStyle(`.xedx-test-style { color: red; border: 1px solid limegreen; background: yellow; }`);
 
     log("quick jquery test");
     log("sidebarroot: ", $('#sidebarroot').length);
