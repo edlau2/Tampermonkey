cd2fe76d0bdc9cdbc3105d2cb46fc545a3382c8c
edlau2 <edlau@ntplx.net>
2025-08-23 15:05:59 -0400

    Delete StatTracker directory

--- StatTracker/StatTrackerHelper.js
+++
@@ -1,6 +0,0 @@
-
-const handleClick = function(ev) {console.log('[handleClick] ev: ', ev);};
-let checkboxes = document.getElementsByTag('input');
-for (let i=0; i < checkboxes.length; i++) {
-  checkboxes[i].addEventListener('click', handleClick);
-}
