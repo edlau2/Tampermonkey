b765bf1b9eff91159b7f17813d735447abfcdc21
edlau2 <edlau@ntplx.net>
2026-01-25 10:40:03 -0500

    Add files via upload

---
+++ BlackBackground/Torn Black Background-1.2.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Torn Black Background
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  This script does...
+// @author       xedx [2100735]
+// @match        https://www.torn.com/*
+// @exclude      https://www.torn.com/recaptcha.php*
+// @icon         https://www.google.com/s2/favicons?domain=torn.com
+// @grant        unsafeWindow
+// ==/UserScript==
+
+/*eslint no-unused-vars: 0*/
+/*eslint no-undef: 0*/
+/*eslint curly: 0*/
+/*eslint no-multi-spaces: 0*/
+
+(function() {
+    'use strict';
+
+    const rootStyles = window.getComputedStyle(document.documentElement);
+    let mainColor = rootStyles.getPropertyValue('--main-bg');
+    console.log("[Torn Black Background] set --main-bg, before: ", mainColor);
+
+    var userStatus;
+    var lastStatus;
+
+    var userData = JSON.parse(sessionStorage.getItem('headerData'));
+    if (userData && userData.user && userData.user.state)
+        userStatus = userData.user.state.status;
+
+    console.log("[Torn Black Background] status: ", userStatus);
+
+//     console.log("[Torn Black Background] userData: ", userData);
+//     console.log("[Torn Black Background] status: ", (userData ? (userData.user ? (userData.user.state ? userData.user.state.status :'no state') : 'no user') : 'no data'));
+
+    $("#body").css("background", "#000");
+    setInterval(updateStatus, 1000);
+
+    function updateStatus() {
+        var userData = JSON.parse(sessionStorage.getItem('headerData'));
+        if (userData && userData.user && userData.user.state)
+            userStatus = userData.user.state.status;
+
+        if (userStatus && lastStatus && userStatus != lastStatus) {
+            console.log("[Torn Black Background] update: ", userStatus, lastStatus);
+            switch (userStatus) {
+                case 'ok':
+                    $("#body").css("background", "#000");
+                    break;
+                case 'hospital':
+                    $("#body").css("background", "#444");
+                    break;
+                default:
+                    console.log("Unknown status: ", userStatus);
+                    $("#body").css("background", "#000");
+                    break;
+            }
+        }
+        lastStatus = userStatus;
+    }
+
+})();
