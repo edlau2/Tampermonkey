350264385f61fd2916d11a100b7f7cec83f91838
edlau2 <edlau@ntplx.net>
2026-01-28 20:01:10 -0500

    Add files via upload

---
+++ ArmoryHighlights/Torn Armory Highlights-0.3.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Torn Armory Highlights
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  This script highlights Revits and Warlords in the fac armory
+// @author       xedx [2100735]
+// @match        https://www.torn.com/factions.php*
+// @icon         https://www.google.com/s2/favicons?domain=torn.com
+// @connect      api.torn.com
+// @require      https://raw.githubusercontent.com/edlau2/Tampermonkey/master/helpers/Torn-JS-Helpers.js
+// @run-at       document-end
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// ==/UserScript==
+
+/*eslint no-undef: 0*/
+
+(function() {
+    'use strict';
+
+    logScriptStart();
+
+    //function log(...data) {console.log(GM_info.script.name + ': ', ...data);}
+    function handleHashChange() {setTimeout(getItemList, 250);}
+
+    // console.log(GM_info.script.name + ' version ' +
+    //     GM_info.script.version + ' script started');
+
+    window.addEventListener('hashchange', handleHashChange);
+
+    function checkPageParams() {
+        let params = new URLSearchParams(location.search);
+        if (params.get('step') != 'your') return log("Not your armory page!");
+        let params2 = location.hash.length ? new URLSearchParams(location.hash.replace("#/", "?")) : null;
+        if (params2 && params2.get('sub') != 'weapons') return log("Not on weapons page!");
+        return true;
+    }
+
+    GM_addStyle(`
+            .xrevit {
+                background-color: rgba(0,195,0,.07);
+            }
+            .xwarlord {
+                background-color: rgba(195,0,0,.07);
+            }
+        `);
+
+    getItemList();
+
+    function getItemList(retries=0) {
+        if (checkPageParams() != true) return log("Going home.");
+        let list = $(".armoury-weapons-wrap > .item-list"); //$("#armoury-weapons > ul.item-list");
+        if (!$(list).length) {
+            if (retries++ < 40) return setTimeout(getItemList, 500, retries);
+            return log("Couldn't find item list!");
+        }
+
+        $(".bonus-attachment-warlord").closest('ul').closest('li').addClass("xwarlord");
+        $(".bonus-attachment-revitalize").closest('ul').closest('li').addClass("xrevit");
+    }
+
+})();
